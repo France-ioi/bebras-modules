0ec0d3cf6dd7ee8a90c3bbcdf1b13444ec79d8a7
mathias-hiron <mathias.hiron@gmail.com>
2015-11-26 15:41:32 +0100

    add fonts to list of images

--- integrationAPI.01/installationAPI.01/pemFioi/installation.js
+++ integrationAPI.01/installationAPI.01/pemFioi/installation.js
@@ -155,7 +155,7 @@ $(document).ready(function() {
 
 
 function fillImages(text, images, res) {
-   var extensions = ["png", "jpg", "gif"];
+   var extensions = ["png", "jpg", "gif", "ttf", "woff", "eot"];
    for (var iExt = 0; iExt < extensions.length; iExt++) {
       var ext = extensions[iExt];
       var regexp = new RegExp("[\'\"]([^\"\']*." + ext + ")[\'\"]", "g");
