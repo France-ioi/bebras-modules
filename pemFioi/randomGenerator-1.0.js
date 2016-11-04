9134abf954319bf7e645a4a91b8c5a38743b6f99
Nir Lavee <nir-lavee@users.noreply.github.com>
2016-11-04 12:26:14 +0200

    Add option to shuffle safely (at most N/2 fixed points).

--- pemFioi/randomGenerator-1.0.js
+++ pemFioi/randomGenerator-1.0.js
@@ -23,6 +23,40 @@ var RandomGenerator = function(initialSeed) {
       Beav.Array.shuffle(array, this.nextInt(0, 100000));
    };
 
+   this.safeShuffle = function(array) {
+      var permutation = this.generateSafePermutation(array.length);
+      var backup = $.extend([], array);
+      for(var index = 0; index < array.length; index++) {
+         array[index] = backup[permutation[index]];
+      }
+   };
+
+   this.generateSafePermutation = function(numElements) {
+      if(numElements <= 1) {
+         return [0];
+      }
+      var permutation = Beav.Array.init(numElements, function(index) {
+         return index;
+      });
+      for(var attempt = 0; attempt < 100; attempt++) {
+         this.shuffle(permutation);
+         if(countFixedPoints(permutation) < numElements / 2) {
+            break;
+         }
+      }
+      return permutation;
+   };
+
+   function countFixedPoints(permutation) {
+      var result = 0;
+      for(var index = 0; index < permutation.length; index++) {
+         if(permutation[index] === index) {
+            result++;
+         }
+      }
+      return result;
+   }
+
    this.getInitialSeed = function() {
       return this.initialSeed;
    };
