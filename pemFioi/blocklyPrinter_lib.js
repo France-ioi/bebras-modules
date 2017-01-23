9c2985f057647691180912f8510490a5813bfd92
Robert Czechowski <czechowski@bwinf.de>
2017-01-23 16:42:52 +0100

    Add Blockly Turtle and Printer library

---
+++ pemFioi/blocklyPrinter_lib.js
@@ -0,0 +1,217 @@
+"use strict";
+
+var getContext = function(display, infos) {
+   var languageStrings = {
+      de: {
+         label: {
+            print: "schreibe",
+            read: "lese",
+            readInteger: "lese ganze Zahl",
+            readFloat: "lese Komma-Zahl",
+            eof: "Ende der Eingabe",
+         },
+         code: {
+            print: "schreibe",
+            read: "lese",
+            readInteger: "leseGanzzahl",
+            readFloat: "leseKommazahl",
+            eof: "eingabeEnde",
+         }
+      }
+   }
+   
+   var strings = languageStrings[stringsLanguage];
+   
+   var cells = [];
+   var texts = [];
+   var scale = 1;
+
+   var context = {
+      display: display,
+      infos: infos,
+      strings: strings,
+      printer: {
+         input_text : "",
+         output_text : "",
+      }
+   };
+
+   context.changeDelay = function(newDelay) {
+      infos.actionDelay = newDelay;
+   };
+
+   context.waitDelay = function(callback, value) {
+      context.runner.waitDelay(callback, value, infos.actionDelay);
+   };
+
+   context.callCallback = function(callback, value) { // Default implementation
+      context.runner.noDelay(callback, value);
+   };
+
+   context.nbRobots = 1;
+
+   context.getRobotItem = function(iRobot) {
+      var items = context.getItems(undefined, undefined, {category: "robot"});
+      return items[iRobot];
+   };
+
+
+   context.program_end = function(callback) {
+      var curRobot = context.curRobot;
+      if (!context.programEnded[curRobot]) {
+         context.programEnded[curRobot] = true;
+         infos.checkEndCondition(context, true);
+      }
+      context.waitDelay(callback);
+   };
+
+   context.reset = function(taskInfos) {
+      if (context.display) {
+         context.resetDisplay();
+      } else {
+         // resetItems();
+      }
+      
+      context.printer.output_text = "";
+      context.printer.input_text = "";
+      
+      if (taskInfos) {
+         context.taskInfos = taskInfos;
+      }
+      if (context.taskInfos.input) {
+         context.printer.input_text = context.taskInfos.input;
+      }
+      
+//      context.printer.input_text = "1\n1\n1\n1\n1\n1\n1\n1\n1\n";
+      
+      //resetScores();
+      context.updateScale();
+   };
+
+   context.resetDisplay = function() {
+      this.delayFactory.destroyAll();
+
+      $("#grid").html("<div style='width:400px; margin:0; padding: 0; overflow:hidden;text-align:left;'><div style='width:175px;height:200px;padding:5px; margin:5px; border: 1px solid black;overflow-y:auto;float:right;'><div style='font-size:small'>Output:</div><pre id='output' style='margin:0px;'>a</pre></div><div style='width:175px;height:200px;padding:5px; margin:5px; border: 1px solid black;overflow-y:auto;float:right;'><div style='font-size:small'>Input:</div><pre id='input' style='margin:0px;'>a</pre></div><div>")
+      
+      
+      $("#output").html("");
+      $("#input").html("");
+      context.blocklyHelper.updateSize();
+      context.updateScale();
+   };
+
+   context.unload = function() {
+      if (context.display) {
+      }
+   };
+
+   context.printer.print = function(value, callback) {
+      if (context.lost) {
+         return;
+      }
+      context.printer.output_text += value + "\n";
+      context.updateScale();
+      context.waitDelay(callback);
+   }
+
+   context.printer.commonRead = function() {
+      var result = "";
+      var index = context.printer.input_text.indexOf('\n');
+      
+      if (index < 0) {
+         result = context.printer.input_text;
+         context.printer.input_text = "";
+      }
+      else {
+         result = context.printer.input_text.substring(0,index);
+         context.printer.input_text = context.printer.input_text.substring(index+1);
+      }
+      return result;
+   }
+      
+   context.printer.read = function(callback) {      
+      context.updateScale();
+      context.callCallback(callback, context.printer.commonRead());
+   }
+
+
+   context.printer.readInteger = function(callback) {
+      context.updateScale();
+      context.callCallback(callback, parseInt(context.printer.commonRead()));
+   }
+
+   context.printer.readFloat = function(callback) {
+      context.updateScale();
+      context.callCallback(callback, parseFloat(context.printer.commonRead()));
+   }
+
+   context.printer.eof = function(callback) {
+      var index = context.printer.input_text.indexOf('\n');
+      
+      if (index < 0) {         
+         context.callCallback(callback, true);
+      }
+      context.callCallback(callback, false);
+   }
+
+
+   context.debug_alert = function(message, callback) {
+      message = message ? message.toString() : '';
+      if (context.display) {
+         alert(message);
+      }
+      context.callCallback(callback);
+   };
+
+   context.customBlocks = {
+      printer: {
+         print: [
+            { name: "print", params: [null]},
+         ],
+         read:  [
+            { name: "read", yieldsValue: true },
+            { name: "readInteger", yieldsValue: true },
+            { name: "readFloat", yieldsValue: true },
+            { name: "eof", yieldsValue: true },
+         ],
+      }
+   }
+
+
+   context.getItems = function(row, col, filters) {
+      var listItems = [];
+      for (var iItem = 0; iItem < context.items.length; iItem++) {
+         var item = context.items[iItem];
+         var itemType = infos.itemTypes[item.type];
+         if ((row == undefined) || ((item.row == row) && (item.col == col))) {
+            var accepted = true;
+            for (var property in filters) {
+               var value = filters[property];
+               if ((itemType[property] == undefined) && (value != undefined)) {
+                  accepted = false;
+                  break;
+               }
+               if ((itemType[property] != undefined) && (itemType[property] != value)) {
+                  accepted = false;
+                  break;
+               }
+            }
+            if (accepted) {
+               item.index = iItem;
+               listItems.push(item);
+            }
+         }
+      }
+      return listItems;
+   };
+
+   context.updateScale = function() {
+      if (!context.display) {
+         return;
+      }
+      $("#output").text(context.printer.output_text);
+      $("#input").text(context.printer.input_text);
+   };
+
+   return context;
+}
---
+++ pemFioi/blocklyTurtle_lib.js
@@ -0,0 +1,295 @@
+"use strict";
+
+var makeTurtle = function() {
+   this.reset = function() {
+      this.x = 150;
+      this.y = 150;
+      this.direction = 0;
+      this.paint = true;
+      if (this.drawingContext)
+         this.drawingContext.clearRect(0, 0, 300, 300);
+      if (this.turtle) {
+         this.turtle.style.left= this.x - 11 + "px";
+         this.turtle.style.top= this.y - 13 + "px";
+         this.turtle.style.transform = "none";
+         
+      }
+   }
+   this.reset();
+   
+   this.turn = function(angle) {
+      this.direction += angle*Math.PI/180;
+      if (this.turtle) {
+         this.turtle.style.transform = "none";
+         this.turtle.style.transform = "rotate(" + (-this.direction) + "rad)";
+      }
+   }
+   this.move = function(amount) {
+      this.drawingContext.beginPath();
+      this.drawingContext.moveTo(this.x, this.y);
+      this.x -= amount*10*Math.sin(this.direction);
+      this.y -= amount*10*Math.cos(this.direction);
+      this.drawingContext.lineTo(this.x, this.y);
+      if (this.paint) {
+         this.drawingContext.stroke();
+      }
+      
+      if (this.turtle) {
+         this.turtle.style.left= this.x - 11 + "px";
+         this.turtle.style.top= this.y - 13 + "px";
+      }
+   }
+   this.start_painting = function() {
+      this.paint = true;
+   }
+   this.stop_painting = function() {
+      this.paint = false;
+   }
+   
+   this.set_colour = function(colour) {
+      this.drawingContext.strokeStyle = colour;
+   }
+   this.setDrawingContext = function(drawingContext) {
+      this.drawingContext = drawingContext;
+      this.drawingContext.lineWidth = 3;
+      this.drawingContext.lineCap = 'round'
+   }
+   this.setTurtle = function(turtle) {
+      this.turtle = turtle;
+
+      this.turtle.style.left= this.x - 11 + "px";
+      this.turtle.style.top= this.y - 13 + "px";
+   }
+}
+
+
+var getContext = function(display, infos) {
+   var languageStrings = {
+      fr: {
+          
+         
+      },
+      de: {
+         label: {
+            move: "gehe",
+            turnleft: "drehe nach links",
+            turnright: "drehe nach rechts",
+            turn: "drehe (Grad) ",
+            alert: "messagebox",
+            log: "logge",
+         },
+         code: {
+            move: "gehe",
+            turnleft: "drehe_links",
+            turnright: "drehe_rechts",
+            turn: "drehe",
+            alert: "alert",
+            log: "log",
+         }
+      }
+   };
+   var strings = languageStrings[stringsLanguage];
+   
+   var cells = [];
+   var texts = [];
+   var scale = 1;
+   var paper;
+
+   var context = {
+      display: display,
+      infos: infos,
+      turtle: {displayTurtle : new makeTurtle, solutionTurtle : new makeTurtle, invisibleTurtle : new makeTurtle},
+      strings: strings,
+   };
+
+   context.changeDelay = function(newDelay) {
+      infos.actionDelay = newDelay;
+   };
+
+   context.waitDelay = function(callback, value) {
+      context.runner.waitDelay(callback, value, infos.actionDelay);
+   };
+
+   context.callCallback = function(callback, value) { // Default implementation
+      context.runner.noDelay(callback, value);
+   };
+
+   context.nbRobots = 1;
+
+   context.getRobotItem = function(iRobot) {
+      var items = context.getItems(undefined, undefined, {category: "robot"});
+      return items[iRobot];
+   };
+
+
+   context.debug_alert = function(callback, message) {
+      message = message ? message.toString() : '';
+      if (context.display) {
+         alert(message);
+      }
+      context.callCallback(callback);
+   };
+
+   context.debug_log = function(callback, message) {
+      message = message ? message.toString() : '';
+      if (context.display) {
+         console.log("vvvvv");
+      }
+      console.log(message);
+      if (context.display) {
+         console.log("^^^^^");
+      }
+      context.callCallback(callback);
+   };
+
+   
+
+   context.program_end = function(callback) {
+      var curRobot = context.curRobot;
+      if (!context.programEnded[curRobot]) {
+         context.programEnded[curRobot] = true;
+         infos.checkEndCondition(context, true);
+      }
+      context.waitDelay(callback);
+   };
+
+   context.reset = function(gridInfos) {
+      if (context.display) {
+         context.resetDisplay();
+      } else {
+         // ... ?
+      }
+      
+      if (gridInfos) {
+         context.drawSolution = gridInfos.drawSolution;
+      }
+
+      context.turtle.displayTurtle.setDrawingContext(document.getElementById('displayfield').getContext('2d'));
+      context.turtle.solutionTurtle.setDrawingContext(document.getElementById('solutionfield').getContext('2d'));
+      context.turtle.invisibleTurtle.setDrawingContext(document.getElementById('invisibledisplayfield').getContext('2d'));
+
+      context.turtle.solutionTurtle.reset();
+      context.turtle.invisibleTurtle.reset();
+
+      context.drawSolution(context.turtle.solutionTurtle);
+
+      
+      // ... ?
+   };
+
+   context.resetDisplay = function() {
+      $("#errors").html("");
+
+      $("#grid").html("<div id='output'  style='height: 300px;width: 300px;border: solid 2px;margin: 12px;position:relative;background-color:white;'> <img id='drawinggrid' width='300' height='300' style='width:300px;height:300px;position:absolute;top:0;left:0;opacity: 0.1;filter: alpha(opacity=10);' src='grid5.png'><canvas id='solutionfield' width='300' height='300' style='width:300px;height:300px;position:absolute;top:0;left:0;opacity: 0.2;filter: alpha(opacity=20);'></canvas><canvas id='displayfield' width='300' height='300' style='width:300px;height:300px;position:absolute;top:0;left:0;'></canvas><canvas id='invisibledisplayfield' width='300' height='300' style='width:300px;height:300px;position:absolute;top:0;left:0;visibility:hidden;'></canvas><img id='turtle' src='turtle.svg' style='width: 22px; height: 27px; position:absolute; left: 139px; top: 136px;'></img></div>")
+      
+      context.blocklyHelper.updateSize();
+      context.turtle.displayTurtle.setTurtle(document.getElementById('turtle'));
+      context.turtle.displayTurtle.reset();
+      context.updateScale();
+   };
+
+   context.unload = function() {
+      if (context.display) {
+         // ... clean up necessary?
+      }
+   };
+
+   function callOnAllTurtles(fn) {
+      fn(context.turtle.invisibleTurtle);
+      if (context.display) {
+         fn(context.turtle.displayTurtle);
+      }
+   }
+   
+   context.turtle.move = function(callback) {
+      callOnAllTurtles((turtle) => {
+         turtle.move(5);
+      })
+      
+      context.waitDelay(callback);
+   }
+
+   context.turtle.turn = function(callback, param) {
+      callOnAllTurtles((turtle) => {
+         turtle.turn(param);
+      })
+
+      context.waitDelay(callback);
+   }
+   
+   context.turtle.turnleft = function(callback) {
+      callOnAllTurtles((turtle) => {
+         turtle.turn(90);
+      })
+
+      context.waitDelay(callback);
+   }
+   
+   context.turtle.turnright = function(callback) {
+      callOnAllTurtles((turtle) => {
+         turtle.turn(-90);
+      });
+      
+      context.waitDelay(callback);
+   }
+
+   context.customBlocks = {
+      turtle: {
+         turtle: [
+            { name: "move" },
+            { name: "turnleft" },
+            { name: "turnright" },
+            { name: "turn",     params: [null]},
+         ],
+      },
+      
+      debug: {
+         debug: [
+            { name: "alert", params: [null], handler: context.debug_alert,
+              blocklyXml: "<block type='alert'><value name='PARAM_0'><block type='text'><field name='TEXT'></field></block></value></block>"},
+            { name: "log",   params: [null], handler: context.debug_log,
+              blocklyXml: "<block type='log'><value name='PARAM_0'><block type='text'><field name='TEXT'></field></block></value></block>"},
+         ],
+      },
+   };
+   
+
+   context.getItems = function(row, col, filters) {
+      var listItems = [];
+      for (var iItem = 0; iItem < context.items.length; iItem++) {
+         var item = context.items[iItem];
+         var itemType = infos.itemTypes[item.type];
+         if ((row == undefined) || ((item.row == row) && (item.col == col))) {
+            var accepted = true;
+            for (var property in filters) {
+               var value = filters[property];
+               if ((itemType[property] == undefined) && (value != undefined)) {
+                  accepted = false;
+                  break;
+               }
+               if ((itemType[property] != undefined) && (itemType[property] != value)) {
+                  accepted = false;
+                  break;
+               }
+            }
+            if (accepted) {
+               item.index = iItem;
+               listItems.push(item);
+            }
+         }
+      }
+      return listItems;
+   };
+
+
+
+   context.updateScale = function() {
+      if (!context.display) {
+         return;
+      }
+      // ...
+      
+   };
+
+   return context;
+}
