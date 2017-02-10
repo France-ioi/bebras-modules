f048698f01db68fe04659dabd48e3a5fbaf8f582
Michel Blockelet <blockelet@crans.org>
2017-02-10 11:23:23 +0100

    blockly_lib: Scratch support for operators, variables, until loop

--- ext/scratch/fixes.js
+++ ext/scratch/fixes.js
@@ -1,6 +1,15 @@
 // Change gap between blocks in the toolbox
 Blockly.Flyout.prototype.GAP_Y = 14;
 
+Blockly.Block.prototype.getVariableField = function () {
+  for(var i=0; i<this.childBlocks_.length; i++) {
+    if(this.childBlocks_[i].type == 'data_variablemenu') {
+      return this.childBlocks_[i].getFieldValue('VARIABLE');
+    }
+  }
+  return null;
+};
+
 Blockly.Workspace.prototype.getFlyout = function () { return null; };
 
 Blockly.Workspace.prototype.remainingCapacity = function() {
@@ -147,6 +156,50 @@ Blockly.Blocks['control_if_else'] = {
   }
 };
 
+Blockly.Blocks['control_repeat_until'] = {
+  /**
+   * Block for repeat until a condition becomes true.
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": Blockly.Msg.CONTROLS_WHILEUNTIL_OPERATOR_UNTIL+" %1",
+      "message1": "%1",
+      "message2": "%1",
+      "lastDummyAlign2": "RIGHT",
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "CONDITION",
+          "check": "Boolean"
+        }
+      ],
+      "args1": [
+        {
+          "type": "input_statement",
+          "name": "SUBSTACK"
+        }
+      ],
+      "args2": [
+        {
+          "type": "field_image",
+          "src": Blockly.mainWorkspace.options.pathToMedia + "/c_arrow.svg",
+          "width": 16,
+          "height": 16,
+          "alt": "*",
+          "flip_rtl": true
+        }
+      ],
+      "inputsInline": true,
+      "previousStatement": null,
+      "nextStatement": null,
+      "category": Blockly.Categories.control,
+      "colour": Blockly.Colours.control.primary,
+      "colourSecondary": Blockly.Colours.control.secondary,
+      "colourTertiary": Blockly.Colours.control.tertiary
+    });
+  }
+};
+
 Blockly.Blocks['operator_not'] = {
   /**
    * Block for "not" unary boolean operator.
@@ -159,6 +212,124 @@ Blockly.Blocks['operator_not'] = {
         {
           "type": "input_value",
           "name": "OPERAND",
+          "check": "Boolean"
+        }
+      ],
+      "inputsInline": true,
+      "output": "Boolean",
+      "category": Blockly.Categories.operators,
+      "colour": Blockly.Colours.operators.primary,
+      "colourSecondary": Blockly.Colours.operators.secondary,
+      "colourTertiary": Blockly.Colours.operators.tertiary,
+      "outputShape": Blockly.OUTPUT_SHAPE_HEXAGONAL
+    });
+  }
+};
+
+Blockly.Blocks['data_setvariableto'] = {
+  /**
+   * Block to set variable to a certain value
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": Blockly.Msg.VARIABLES_SET,
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "VARIABLE"
+        },
+        {
+          "type": "input_value",
+          "name": "VALUE"
+        }
+      ],
+      "previousStatement": null,
+      "nextStatement": null,
+      "category": Blockly.Categories.data,
+      "colour": Blockly.Colours.data.primary,
+      "colourSecondary": Blockly.Colours.data.secondary,
+      "colourTertiary": Blockly.Colours.data.tertiary
+    });
+  }
+};
+
+Blockly.Blocks['data_changevariableby'] = {
+  /**
+   * Block to change variable by a certain value
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": Blockly.Msg.MATH_CHANGE_TITLE,
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "VARIABLE"
+        },
+        {
+          "type": "input_value",
+          "name": "VALUE"
+        }
+      ],
+      "previousStatement": null,
+      "nextStatement": null,
+      "category": Blockly.Categories.data,
+      "colour": Blockly.Colours.data.primary,
+      "colourSecondary": Blockly.Colours.data.secondary,
+      "colourTertiary": Blockly.Colours.data.tertiary
+    });
+  }
+};
+
+Blockly.Blocks['operator_and'] = {
+  /**
+   * Block for "and" boolean comparator.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": "%1 "+Blockly.Msg.LOGIC_OPERATION_AND+" %2",
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "OPERAND1",
+          "check": "Boolean"
+        },
+        {
+          "type": "input_value",
+          "name": "OPERAND2",
+          "check": "Boolean"
+        }
+      ],
+      "inputsInline": true,
+      "output": "Boolean",
+      "category": Blockly.Categories.operators,
+      "colour": Blockly.Colours.operators.primary,
+      "colourSecondary": Blockly.Colours.operators.secondary,
+      "colourTertiary": Blockly.Colours.operators.tertiary,
+      "outputShape": Blockly.OUTPUT_SHAPE_HEXAGONAL
+    });
+  }
+};
+
+Blockly.Blocks['operator_or'] = {
+  /**
+   * Block for "or" boolean comparator.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": "%1 "+Blockly.Msg.LOGIC_OPERATION_OR+" %2",
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "OPERAND1",
+          "check": "Boolean"
+        },
+        {
+          "type": "input_value",
+          "name": "OPERAND2",
           "check": "Boolean"
         }
       ],
@@ -187,7 +358,6 @@ Blockly.JavaScript['control_if'] = function(block) {
 
 Blockly.JavaScript['control_if_else'] = function(block) {
   // If/then/else condition.
-  var n = 0;
   var argument = Blockly.JavaScript.valueToCode(block, 'CONDITION',
       Blockly.JavaScript.ORDER_NONE) || 'false';
   var branch = Blockly.JavaScript.statementToCode(block, 'SUBSTACK');
@@ -224,12 +394,81 @@ Blockly.JavaScript['control_repeat'] = function(block) {
   return code;
 };
 
+Blockly.JavaScript['control_repeat_until'] = function(block) {
+  // If/then/else condition.
+  var n = 0;
+  var argument = Blockly.JavaScript.valueToCode(block, 'CONDITION',
+      Blockly.JavaScript.ORDER_NONE) || 'false';
+  var branch = Blockly.JavaScript.statementToCode(block, 'SUBSTACK');
+  var code = 'while (!(' + argument + ')) {\n' + branch + '}';
+  return code + '\n';
+};
+
 Blockly.JavaScript['operator_not'] = function(block) {
    var b = Blockly.JavaScript.ORDER_LOGICAL_NOT;
    return["!"+ (Blockly.JavaScript.valueToCode(block,"OPERAND",b) || "true"), b]
 };
 
+
+Blockly.JavaScript['data_variable'] = function(block) {
+  // Variable getter.
+  var code = Blockly.JavaScript.variableDB_.getName(block.getVariableField(),
+      Blockly.Variables.NAME_TYPE);
+  return [code, Blockly.JavaScript.ORDER_ATOMIC];
+};
+
+Blockly.JavaScript['data_setvariableto'] = function(block) {
+  // Variable setter.
+  var argument0 = Blockly.JavaScript.valueToCode(block, 'VALUE',
+      Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
+  var varName = Blockly.JavaScript.variableDB_.getName(
+      block.getVariableField(), Blockly.Variables.NAME_TYPE);
+  return varName + ' = ' + argument0 + ';\n';
+};
+
+Blockly.JavaScript['data_changevariableby'] = function(block) {
+  // Increment
+  var argument0 = Blockly.JavaScript.valueToCode(block, 'VALUE',
+      Blockly.JavaScript.ORDER_ASSIGNMENT) || '0';
+  var varName = Blockly.JavaScript.variableDB_.getName(
+      block.getVariableField(), Blockly.Variables.NAME_TYPE);
+  return varName + ' += ' + argument0 + ';\n';
+};
+
+Blockly.JavaScript['operators'] = function(block) {
+  // Generator for all operators
+  var nameToOp = {
+    'operator_add': {op: '+', varname: 'NUM', order: Blockly.JavaScript.ORDER_ADDITION},
+    'operator_subtract': {op: '-', varname: 'NUM', order: Blockly.JavaScript.ORDER_SUBTRACTION},
+    'operator_multiply': {op: '*', varname: 'NUM', order: Blockly.JavaScript.ORDER_MULTIPLICATION},
+    'operator_divide': {op: '/', varname: 'NUM', order: Blockly.JavaScript.ORDER_DIVISION},
+    'operator_equals': {op: '==', varname: 'OPERAND', order: Blockly.JavaScript.ORDER_EQUALITY},
+    'operator_gt': {op: '>', varname: 'OPERAND', order: Blockly.JavaScript.ORDER_RELATIONAL},
+    'operator_lt': {op: '<', varname: 'OPERAND', order: Blockly.JavaScript.ORDER_RELATIONAL},
+    'operator_and': {op: '&&', varname: 'OPERAND', order: Blockly.JavaScript.ORDER_LOGICAL_AND},
+    'operator_or': {op: '||', varname: 'OPERAND', order: Blockly.JavaScript.ORDER_LOGICAL_OR}
+  };
+  var opInfo = nameToOp[block.type];
+  var argument0 = Blockly.JavaScript.valueToCode(block, opInfo.varname+'1', opInfo.order) || '0';
+  if(argument0 == 'NaN') { argument0 = '0'; };
+  var argument1 = Blockly.JavaScript.valueToCode(block, opInfo.varname+'2', opInfo.order) || '0';
+  if(argument1 == 'NaN') { argument1 = '0'; };
+  var code = argument0 + ' ' + opInfo.op + ' ' + argument1;
+  return [code, opInfo.order];
+}
+
+Blockly.JavaScript['operator_add'] = Blockly.JavaScript['operators'];
+Blockly.JavaScript['operator_subtract'] = Blockly.JavaScript['operators'];
+Blockly.JavaScript['operator_multiply'] = Blockly.JavaScript['operators'];
+Blockly.JavaScript['operator_divide'] = Blockly.JavaScript['operators'];
+Blockly.JavaScript['operator_equals'] = Blockly.JavaScript['operators'];
+Blockly.JavaScript['operator_gt'] = Blockly.JavaScript['operators'];
+Blockly.JavaScript['operator_lt'] = Blockly.JavaScript['operators'];
+Blockly.JavaScript['operator_and'] = Blockly.JavaScript['operators'];
+Blockly.JavaScript['operator_or'] = Blockly.JavaScript['operators'];
+
 // TODO :: Python generation
 Blockly.Python['control_if'] = function(block) { return '# Error: control_if not implemented in Python!'; }
 Blockly.Python['control_if_else'] = function(block) { return '# Error: control_if_else not implemented in Python!'; }
 Blockly.Python['control_if_repeat'] = function(block) { return '# Error: control_repeat not implemented in Python!'; }
+Blockly.Python['operator_not'] = function(block) { return '# Error: operator_not not implemented in Python!'; }
