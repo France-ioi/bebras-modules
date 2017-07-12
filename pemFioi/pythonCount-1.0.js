34ecb7b9054050def0e341e0bc14f1c053d7b443
mathias-hiron <mathias.hiron@gmail.com>
2017-07-12 14:11:35 +0200

    fix includeAll

--- pemFioi/pythonCount-1.0.js
+++ pemFioi/pythonCount-1.0.js
@@ -107,10 +107,7 @@ function pythonForbiddenLists(includeBlocks) {
       }
    };
 
-   if(includeBlocks && includeBlocks.standardBlocks) {
-      if(includeBlocks.standardBlocks.includeAll) {
-         return false;
-      }
+   if(includeBlocks && includeBlocks.standardBlocks && !includeBlocks.standardBlocks.includeAll) {
       if(includeBlocks.standardBlocks.wholeCategories) {
          for(var c=0; c<includeBlocks.standardBlocks.wholeCategories.length; c++) {
             var categoryName = includeBlocks.standardBlocks.wholeCategories[c];
