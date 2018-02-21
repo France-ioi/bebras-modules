e8df3e6e9ae1807d1667de97bc9f599ba0744f66
Sylvain Chiron <chironsylvain@orange.fr>
2018-02-21 14:18:45 +0100

    Add math_angle block
    Signed-off-by: Sylvain Chiron <chironsylvain@orange.fr>

--- pemFioi/quickAlgo/blockly_blocks.js
+++ pemFioi/quickAlgo/blockly_blocks.js
@@ -1713,6 +1713,21 @@ function getBlocklyBlockFunctions(maxBlocks, nbTestCases) {
          Blockly.JavaScript['controls_untilWhile'] = Blockly.JavaScript['controls_whileUntil'];
          Blockly.Python['controls_untilWhile'] = Blockly.Python['controls_whileUntil'];
 
+         Blockly.Blocks['math_angle'] = {
+            init: function() {
+               this.setOutput(true, 'Number');
+               this.appendDummyInput()
+                   .appendField(new Blockly.FieldAngle(90), "ANGLE");
+               this.setColour(Blockly.Blocks.math.HUE);
+            }
+         };
+         Blockly.JavaScript['math_angle'] = function(block) {
+           return ['' + block.getFieldValue('ANGLE'), Blockly.JavaScript.ORDER_FUNCTION_CALL];
+         };
+         Blockly.Python['math_angle'] = function(block) {
+           return ['' + block.getFieldValue('ANGLE'), Blockly.Python.ORDER_FUNCTION_CALL];
+         };
+
          Blockly.Blocks['math_extra_single'] = {
            /**
             * Block for advanced math operators with single operand.
