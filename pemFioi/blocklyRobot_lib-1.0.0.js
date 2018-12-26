47cb9a9513247cfe39a634e7924ed7db15cc1080
Michel Blockelet <blockelet@crans.org>
2018-12-26 21:23:35 +0100

    quickAlgo: Keep old grid scaling when not using new interface

--- pemFioi/blocklyRobot_lib-1.0.0.js
+++ pemFioi/blocklyRobot_lib-1.0.0.js
@@ -2404,8 +2404,13 @@ var getContext = function(display, infos, curLevel) {
       if(paper == null) {
          return;
       }
-      var areaWidth = Math.max(200, $('#grid').width()-24);
-      var areaHeight = Math.max(150, $('#grid').height()-24);
+      if(window.quickAlgoResponsive) {
+         var areaWidth = Math.max(200, $('#grid').width()-24);
+         var areaHeight = Math.max(150, $('#grid').height()-24);
+      } else {
+         var areaWidth = 400;
+         var areaHeight = 600;
+      }
       var newCellSide = 0;
       if(context.nbCols && context.nbRows) {
          var marginAsCols = infos.leftMargin / infos.cellSide;
