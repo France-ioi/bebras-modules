8039f63d9bdc60039748d5e4fa1cb11ecc0b7186
jpmendoza <jpablo@gmail.com>
2019-03-20 10:45:18 -0600

    Make strings translatable, do not trigger a grade if the manual task ended

--- pemFioi/quickAlgo/subtask.js
+++ pemFioi/quickAlgo/subtask.js
@@ -238,8 +238,11 @@ var initBlocklySubTask = function(subTask, language) {
          // Log the attempt
          subTask.logActivity();
          // Launch an evaluation after the execution
-         subTask.context.display = false;
-         subTask.getGrade(handleResults, true, subTask.iTestCase);
+
+         if (!subTask.context.DoNotGrade) {
+            subTask.context.display = false;
+            subTask.getGrade(handleResults, true, subTask.iTestCase);
+         }
       });
       initContextForLevel(initialTestCase);
    };
