fc4e5a0dd0589bbee75332718a9e3f5c461dc65f
mobydimk <mobydimk@gmail.com>
2019-04-29 04:47:56 +0400

    quiz regexp validation update

--- pemFioi/quiz/ui.js
+++ pemFioi/quiz/ui.js
@@ -161,23 +161,29 @@ function QuizUI(params) {
             validator = '^-?[0-9]*\.?[0-9]*$';
         } else if(format == 'regexp') {
             validator = answer.attr('validator');
+            var code = 'validator = ' + validator;
+            try {
+                validator = eval(code);
+            } catch(e) {}
         }
         if(validator) {
             var reg = new RegExp(validator);
-            input.on('change', function() {
-                answer.removeClass('mistake');
-                var el = $(this);
-                var valid = reg.test(el.val());
-                input.toggleClass('error', !valid);
-                var msg = answer.find('.error-message');
-                !valid && !msg.length && answer.append(
-                    '<div class="error-message">' +
-                    '<i class="fas fa-bell icon"></i>' +
-                    lang.translate('error_' + format) +
-                    '</div>'
-                );
-                valid && msg && msg.remove();
-            });
+            if(typeof reg === 'object' && 'test' in reg) {
+                input.on('change blur', function() {
+                    answer.removeClass('mistake');
+                    var el = $(this);
+                    var valid = reg.test(el.val());
+                    input.toggleClass('error', !valid);
+                    var msg = answer.find('.error-message');
+                    !valid && !msg.length && answer.append(
+                        '<div class="error-message">' +
+                        '<i class="fas fa-bell icon"></i>' +
+                        lang.translate('error_' + format) +
+                        '</div>'
+                    );
+                    valid && msg && msg.remove();
+                });
+            }
         }
         input.attr('placeholder', lang.translate('placeholder_' + format));
         answer.append(input);
