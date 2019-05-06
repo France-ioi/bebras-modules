11f42ec42de0ebaa6dff544caf7d60c5f370a5bb
UncleBone <arnaud.lgrd@gmail.com>
2019-05-06 22:01:31 +0200

    language-theory-1.0.js: add several functions

--- pemFioi/language-theory-1.0.js
+++ pemFioi/language-theory-1.0.js
@@ -1,11 +1,16 @@
-function LanguageTheory(){
+function LanguageTheory(rng){
 
-   this.getRandomWord = function(minLength,maxLength,rng) {
+   this.rng = rng;
+
+   this.setRNG = function(fct) {
+      this.rng = fct;
+   };
+
+   this.getWordList = function(minLength,maxLength) {
       var wordList = [];
       var array = [];
       var minLength = minLength || 0;
       var maxLength = maxLength || Infinity;
-
       for(var nounType of nounTypes){
          if(nounType != "city"){
             array = array.concat(nouns[nounType]["M"]).concat(nouns[nounType]["F"]);
@@ -21,12 +26,18 @@ function LanguageTheory(){
          }
       }
 
-      var index = (rng.nextInt(0,10000) + Math.floor(Math.random()*10000))%(wordList.length-1);
-      return wordList[index];
+      return wordList;
+   };
+   
+   this.getRandomWord = function(minLength,maxLength) {
+      var wordList = this.getWordList(minLength,maxLength);
+      var index = (this.rng.nextInt(0,10000) + Math.floor(Math.random()*10000))%(wordList.length-1);
+      var word = this.formatWord(wordList[index]);
+      return word;
    };
 
-   this.getWord = function(minLength,maxLength,rng) {
-      var word = this.getRandomWord(minLength,maxLength,rng).toLowerCase().trim();
+   this.formatWord = function(word) {
+      word = word.toLowerCase().trim();
       word = word.replace(/[èéêë]/g,"e");
       word = word.replace(/[ôö]/g,"o");
       word = word.replace(/[âà]/g,"a");
@@ -34,6 +45,61 @@ function LanguageTheory(){
       word = word.replace(/[û]/g,"u");
       word = word.toUpperCase();
       return word;
+   };
+
+   this.getWords = function(minLength,maxLength,nbWords) {
+      var wordList = this.getWordList(minLength,maxLength);
+      // this.rng.shuffle(wordList);
+      Beav.Array.shuffle(wordList,this.rng.nextInt(0,10000) + Math.floor(Math.random()*10000));
+      var words = [];
+      for(var iWord = 0; iWord < nbWords; iWord++){
+         words.push(this.formatWord(wordList[iWord]));
+      }
+      return words;
+   };
+
+   this.getRole = function(word,role) {
+      switch(role) {
+         case "prefix":
+            return word.slice(0,this.rng.nextInt(2,word.length - 1));
+         case "suffix":
+            return word.slice(-this.rng.nextInt(2,word.length - 1));
+         case "factor":
+            return this.getFactor(word);
+         case "subsequence":
+            return this.getSubsequence(word);
+         case "none":
+         default:
+            do{ 
+               var shuffledWord = Array.from(word);
+               this.rng.shuffle(shuffledWord);
+               var stringArray = shuffledWord.slice(0,this.rng.nextInt(2,word.length - 1));
+               var string = stringArray.toString();
+               string = string.replace(/,/gi,"");
+            }while(this.isSubsequence("subsequence",string,word));
+            return string;
+      }
+   };
+
+   this.getFactor = function(word) {
+      var startIndex = this.rng.nextInt(1,word.length - 3);
+      var endIndex = this.rng.nextInt(startIndex + 2, word.length - 1);
+      return word.slice(startIndex,endIndex);
+   };
+
+   this.getSubsequence = function(word) {
+      do{
+         var startIndex = this.rng.nextInt(0,word.length - 3);
+         var endIndex = this.rng.nextInt(startIndex + 1,word.length);
+         var string = word.charAt(startIndex);
+         for(var iLetter = startIndex + 1 ; iLetter < endIndex; iLetter++){
+            if(this.rng.nextBit()){
+               string += word.charAt(iLetter);
+            }
+         }
+         string += word.charAt(endIndex);
+      }while(this.isSubsequence("factor",string,word));
+      return string;
    };
 
    this.validation = function(word,answer,length) {
