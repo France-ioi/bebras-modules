a2e4862cd15116c955feb4f8e737c79a85af23d5
mobydimk <mobydimk@gmail.com>
2019-05-22 16:55:35 +0400

    quiz fill gaps

---
+++ pemFioi/quiz/loader.js
@@ -0,0 +1,42 @@
+function loadQuizModules(modulesPath) {
+    window.modulesPath = modulesPath
+
+    var modules = [
+        'jquery-1.7.1',
+        'JSON-js',
+        'raphael-2.2.1',
+        'jschannel',
+        'platform-pr',
+        'installationAPI.01',
+        'miniPlatform',
+        'quiz_styles',
+        'quiz',
+        'quiz_task',
+        'quiz_grader',
+    ];
+
+    // question types
+    if(window.quiz_question_types.fill_gaps) {
+        modules.push('jquery-ui-1.10.3');
+        modules.push('quiz_questions_fill_gaps');
+    }
+    if(window.quiz_question_types.single || window.quiz_question_types.multiple) {
+        modules.push('quiz_questions_choice');
+    }
+    if(window.quiz_question_types.input) {
+        modules.push('quiz_questions_input');
+    }
+
+    // text processing
+    if(quiz_settings.mathjax) {
+        modules.push('mathjax');
+    }
+    if(task_data_info.markdown) {
+        modules.push('showdown');
+    }
+    if(task_data_info.markdown || quiz_settings.mathjax) {
+        modules.push('post_processor');
+    }
+
+    importModules(modules);
+}
