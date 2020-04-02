80c9d778a528eebb871136678897412bd4774e1b
Pablo Mendoza <jpablo@gmail.com>
2020-04-02 15:12:30 -0600

    quickPi: add polyfill for OffscreenCanvas

--- pemFioi/quickpi/blocklyQuickPi_screen.js
+++ pemFioi/quickpi/blocklyQuickPi_screen.js
@@ -1,4 +1,22 @@
-﻿class screenImageData {
+﻿if (!window.OffscreenCanvas) {
+    window.OffscreenCanvas = class OffscreenCanvas {
+      constructor(width, height) {
+        this.canvas = document.createElement("canvas");
+        this.canvas.width = width;
+        this.canvas.height = height;
+  
+        this.canvas.convertToBlob = () => {
+          return new Promise(resolve => {
+            this.canvas.toBlob(resolve);
+          });
+        };
+  
+        return this.canvas;
+      }
+    };
+  }
+
+class screenImageData {
     constructor() {
         this.isDrawingData = true;
         this.imagedata = [];
