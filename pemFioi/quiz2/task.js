3415825eba61995d0335199a71bf991bc38a0b67
mobydimk <mobydimk@gmail.com>
2020-04-25 18:29:24 +0400

    quiz2 remove debug

--- pemFioi/quiz2/task.js
+++ pemFioi/quiz2/task.js
@@ -315,14 +315,14 @@
 
             task.getAnswer = function(callback) {
                 var answer = JSON.stringify(q.getAnswer());
-                console.log('task.getAnswer', answer)
+                //console.log('task.getAnswer', answer)
                 callback(answer);
             };
 
 
             task.reloadAnswer = function(answer, callback) {
                 try {
-                    console.log('task.reloadAnswer', answer)
+                    //console.log('task.reloadAnswer', answer)
                     answer = JSON.parse(answer);
                     q.setAnswer(answer);
                 } catch(e) {
