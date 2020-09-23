3f3ef0eb0ad559a4a0f8239a2e833b7c13a2f27e
mobydimk <mobydimk@gmail.com>
2020-09-23 19:18:59 +0400

    gaps table

--- pemFioi/components/gaps-table/component.js
+++ pemFioi/components/gaps-table/component.js
@@ -10,9 +10,14 @@ function GapsTable(params) {
         valid: [[]],
 
         cell_min_width: 100,
+
         table_min_size: {
             rows: 2,
             cols: 2
+        },
+        table_max_size: {
+            rows: 10,
+            cols: 10
         }
     }
     params = Object.assign({}, defaults, params);
@@ -34,44 +39,325 @@ function GapsTable(params) {
         return a;
     }
 
+    function iterateTableArray(arr, callback) {
+        for(var i=0; i<arr.length; i++) {
+            for(var j=0; j<arr[i].length; j++) {
+                callback(i, j, arr[i][j]);
+            }
+        }
+    }
+
+    function makeArray(length, value) {
+        return new Array(length).fill(value);
+    }
+
+    function getTableArraySize(arr) {
+        var res = {
+            rows: arr.length,
+            cols: 0
+        }
+        for(var i=0; i<arr.length; i++) {
+            res.cols = Math.max(res.cols, arr[i].length);
+        }        
+        return res;
+    }    
+
+    function alignTableArray(arr, default_value) {
+        var size = getTableArraySize(arr);
+        for(var i=0; i<arr.length; i++) {
+            arr[i] = arr[i].concat(makeArray(size.cols - arr[i].length, default_value));    
+        }
+        return arr;
+    }
+
+
+
+
+
 
     var wrapper = $('<div class="gaps-table"/>');
     params.parent.append(wrapper);
 
 
-    // init values if empty
-    var resizable = false;
-    if(!params.values) {
-        params.header = false;
-        resizable = true;
-        params.values = [];
-        for(var i=0; i<params.valid.length; i++) {
-            var row = [];
-            for(var j=0; j<params.valid[i].length; j++) {
-                row.push(params.valid[i][j] !== '' ? params.placeholder : '');
+
+
+
+
+
+    function Schema(data) {
+
+        if(Array.isArray(data)) {
+            data = alignTableArray(data, '')
+        }
+
+        return {
+            isStatic: function() {
+                return !!data;
+            },
+
+            isPlaceholder: function(row, col) {
+                return data ? data[row][col] === params.placeholder : true;
+            },
+
+            getContent: function(row, col) {
+                return data ? data[row][col] : '';
             }
-            params.values.push(row);
         }
     }
+
+
+
+
+    function Table(data) {
+
+        var cells = [];
+        var table = $('<table/>');
+        var table_outline = $('<div class="table-outline"/>').append(table);
+        var table_container = $('<div class="table-container"/>').append(table_outline);
+        wrapper.append(table_container);
+
+        var schema = Schema(data.schema);
+       
+
+
+        function createCell(row, col) {
+            var is_header = row == 0 && params.header;
+            var cell = $(is_header ? '<th/>' : '<td/>');
+            cell.css({
+                'min-width': params.cell_min_width + 'px',
+            });
+            if(schema.isPlaceholder(row, col)) {
+                cell.addClass('placeholder');
+                cell.droppable({
+                    scope: uid,
+                    hoverClass: 'placeholder-hover',
+                    drop: function(event, ui) {
+                        resetMistakes();                        
+                        toolbar.append(cell.find('.value').first());
+                        ui.draggable.detach().css({top: 0,left: 0}).appendTo(cell);
+                    }
+                });
+            } else {
+                cell.html(schema.getContent(row, col));
+            }
+            return cell;
+        }
+
+        function createRow(row, length) {
+            cells[row] = [];
+            var tr = $('<tr/>');
+            for(var i=0; i<length; i++) {
+                var cell = createCell(row, i);
+                tr.append(cell);
+                cells[row][i] = cell;
+            }
+            table.append(tr);
+        }
+
+
+
+        function resize(cols, rows) {
+            cols = Math.max(cols, params.table_min_size.cols);
+            cols = Math.min(cols, params.table_max_size.cols);
+            rows = Math.max(rows, params.table_min_size.rows);
+            rows = Math.min(rows, params.table_max_size.rows);
+
+            
+
+            // expand height
+            while(cells.length < rows) {
+                createRow(cells.length, cols)
+            }
+
+            // collapse height
+            if(cells.length > rows) {
+                for(var i=rows; i<cells.length; i++) {
+                    var tr = cells[i][0].parent();
+                    for(var j=0; j<cells[i].length; j++) {
+                        toolbar.append(cells[i][j].find('.value'));
+                        cells[i][j].remove();
+                    }
+                    tr.remove();
+                }
+                cells = cells.slice(0, rows);
+            }
+
+            if(!cells.length || cells[0].length == cols) {
+                return;
+            }        
+
+            
+            for(var i=0; i<cells.length; i++) {
+                var tr = cells[i][0].parent();
+                if(cells[i].length < cols) {
+                    // expand width
+                    var l = cells[i].length;                            
+                    for(var j=l; j<cols; j++) {
+                        cells[i][j] = createCell(i, j);
+                        tr.append(cells[i][j]);
+                    }
+                } else if (cells[i].length > cols) {
+                    // collapse width
+                    for(var j=cols; j<cells[i].length; j++) {
+                        toolbar.append(cells[i][j].find('.value'));
+                        cells[i][j].remove();
+                    }
+                    cells[i] = cells[i].slice(0, cols);
+                }
+            }
+        }
+
+
+
+        if(schema.isStatic()) {
+            for(var i=0; i<data.values.length; i++) {
+                createRow(i, data.values[i].length);
+            }
+        } else {
+            for(var i=0; i<params.table_min_size.rows; i++) {
+                createRow(i, params.table_min_size.cols);
+            }
+
+            table_outline.resizable({
+                stop: function(event, ui) {
+                    var cols = 0;
+                    var w = 0;
+                    while(w < ui.size.width) {
+                        if(cells.length && cols < cells[0].length) {
+                            w += cells[0][cols].outerWidth();
+                        } else {
+                            w += params.cell_min_width;
+                        }
+                        cols++;
+                    }
+                    var rows = 0;
+                    var h = 0;
+                    while(h < ui.size.height) {
+                        if(rows < cells.length) {
+                            h += cells[rows][0].outerHeight();
+                        } else {
+                            h += 30; // TODO: add to config somehow ?
+                        }
+                        rows++;
+                    }                
+                    resize(cols - 1, rows - 1);
+                    table_outline.width('');
+                    table_outline.height('');                
+                }
+            });
+        }
+    
+
+
+        function getAnswer() {
+            var res = [];
+            for(var i=0; i<cells.length; i++) {
+                res[i] = [];
+                for(var j=0; j<cells[i].length; j++) {
+                    res[i][j] = schema.isPlaceholder(i, j) ? cells[i][j].text() : '';
+                }
+            }
+            return res;
+        }
+    
+    
+        function setAnswer(answer) {
+            if(!schema.isStatic()) {
+                var size = getTableArraySize(answer);
+                resize(size.rows, size.cols);
+            }
+            var values = toolbar.find('.value').toArray();
+            for(var i=0; i<answer.length; i++) {
+                for(var j=0; j<answer[i].length; j++) {
+                    if(!schema.isPlaceholder(i, j)) {
+                        continue;
+                    }
+                    for(var k=0; k<values.length; k++) {
+                        if(values[k].innerText === answer[i][j]) {
+                            cells[i][j].append(values[k]);
+                            values.splice(k, 1);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+
+        function resetMistakes() {
+            table.find('td').removeClass('mistake');
+        }
+    
+    
+        function resetValues() {
+            toolbar.append(table.find('.value'));
+        }
+    
+
+        function validate(silent) {
+            var res = true;
+            var valid;
+            for(var i=0; i<cells.length; i++) {
+                for(var j=0; j<cells[i].length; j++) {
+                    if(i >= data.values.length || j > data.values[i].length) {
+                        valid = false;
+                    } else if(!schema.isPlaceholder(i, j)) {
+                        continue;
+                    } else {
+                        var v1 = data.values[i][j];
+                        var v2 = cells[i][j].text();
+                        valid = v1 !== '' && v1 === v2;
+                    }
+                    cells[i][j].toggleClass('mistake', !valid);
+                    res = res && valid;
+                }
+            }
+            return res;
+        }        
+
+
+        function getValues() {
+            var res = [];
+            iterateTableArray(data.values, function(row, col, v) {
+                if(schema.isPlaceholder(row, col)) {
+                    res.push(v);
+                }
+            })
+            return res;
+        }
+
+
+        return {
+            getAnswer: getAnswer,
+            setAnswer: setAnswer,
+            getValues: getValues,
+            validate: validate,
+            resetValues: resetValues
+        }
+
+    }
+
+
+
+    // tables
+    var tables = [];
+    var values = [];
+    for(var i=0; i<params.tables.length; i++) {
+        var table = Table(params.tables[i]);
+        tables.push(table);
+        values = values.concat(table.getValues());
+    }
+
 
 
     // toolbar
-    var values = [];
-    for(var i=0; i<params.valid.length; i++) {
-        for(var j=0; j<params.valid[i].length; j++) {
-            var v = params.valid[i][j];
-            if(v !== '' && v !== params.placeholder) {
-                values.push(v);
-            }
-        }
-    }
     values = shuffleArray(values);
 
     var toolbar = $('<div class="gaps-toolbar"/>');
     for(var i=0; i<values.length; i++) {
         toolbar.append($('<span class="value">' + values[i] + '</span>'));
     }        
-    wrapper.append(toolbar);
+    wrapper.prepend(toolbar);
 
     toolbar.find('.value').each(function(i) {
         var el = $(this), text = el.text();
@@ -94,217 +380,36 @@ function GapsTable(params) {
 
 
 
-    // table
-    var cells = [];
-    var table = $('<table/>');
-    var table_container = $('<div class="table-container"/>')
-    table_container.append(table);
-
-
-    function createCell(value, is_header) {
-        var cell = $(is_header ? '<th/>' : '<td/>');
-        cell.css({
-            'min-width': params.cell_min_width + 'px',
-        });
-        if(value == params.placeholder) {
-            cell.addClass('placeholder');
-            cell.droppable({
-                scope: uid,
-                hoverClass: 'placeholder-hover',
-                drop: function(event, ui) {
-                    toolbar.append(cell.find('.value').first());
-                    ui.draggable.detach().css({top: 0,left: 0}).appendTo(cell);
-                }
-            });
-        } else if(value !== '') {
-            cell.html(value);
-        }
-        return cell;
-    }
-
-    function renderRow(values, is_header) {
-        var row = $('<tr/>');
-        var cells_row = [];
-        for(var j=0; j<values.length; j++) {
-            var cell = createCell(values[j], is_header);
-            cells_row.push(cell);
-            row.append(cell);
-        }
-        cells.push(cells_row);
-        table.append(row);
-    }
-
-
-    for(var i=0; i<params.values.length; i++) {
-        renderRow(params.values[i], params.header && i == 0)
-    }
-
-    wrapper.append(table_container);
-
-    /*
-    table.find('.placeholder').each(function() {
-        var placeholder = $(this)
-        placeholder.droppable({
-            scope: uid,
-            hoverClass: 'placeholder-hover',
-            drop: function(event, ui) {
-                toolbar.append(placeholder.find('span').first());
-                ui.draggable.detach().css({top: 0,left: 0}).appendTo(placeholder);
-            }
-        });
-    });            
-    */
-
-
-
-    function resize(cols, rows) {
-        cols = Math.max(cols, params.table_min_size.cols);
-        rows = Math.max(rows, params.table_min_size.rows);
-
-        // expand height
-        while(cells.length < rows) {
-            params.values.push(new Array(cols).fill(params.placeholder));
-            var idx = params.values.length - 1;
-            renderRow(params.values[idx], params.header && idx == 0)
-        }
-
-        // collapse height
-        if(cells.length > rows) {
-            for(var i=rows; i<cells.length; i++) {
-                var tr = cells[i][0].parent();
-                for(var j=0; j<cells[i].length; j++) {
-                    toolbar.append(cells[i][j].find('.value'));
-                    cells[i][j].remove();
-                }
-                tr.remove();
-            }
-            cells = cells.slice(0, rows);
-        }
-
-        if(!cells.length || cells[0].length == cols) {
-            return;
-        }        
-
-        
-        for(var i=0; i<cells.length; i++) {
-            var is_header = i == 0 && params.header;
-            var tr = cells[i][0].parent();
-            if(cells[i].length < cols) {
-                // expand width
-                var l = cells[i].length;                            
-                params.values[i] = params.values[i].concat(new Array(cols - l).fill(params.placeholder));
-                for(var j=l; j<cols; j++) {
-                    var cell = createCell(params.values[i][j], is_header);
-                    cells[i][j] = cell;
-                    tr.append(cell);
-                }
-            } else if (cells[i].length > cols) {
-                // collapse width
-                params.values[i] = params.values[i].slice(0, cols);
-                for(var j=cols; j<cells[i].length; j++) {
-                    toolbar.append(cells[i][j].find('.value'));
-                    cells[i][j].remove();
-                }
-                cells[i] = cells[i].slice(0, cols);
-            }
-        }
-    }
-
-
-
-    if(resizable) {
-        table_container.resizable({
-            grid: [params.values[0].length, params.values.length],
-            stop: function(event, ui) {
-                var cols = 0;
-                var w = 0;
-                while(w < ui.size.width) {
-                    if(cells.length && cols < cells[0].length) {
-                        w += cells[0][cols].outerWidth();
-                    } else {
-                        w += params.cell_min_width;
-                    }
-                    cols++;
-                }
-                var rows = 0;
-                var h = 0;
-                while(h < ui.size.height) {
-                    if(rows < cells.length) {
-                        h += cells[rows][0].outerHeight();
-                    } else {
-                        h += 30; // TODO: add to config somehow ?
-                    }
-                    rows++;
-                }                
-                resize(cols - 1, rows - 1);
-                table_container.width('');
-                table_container.height('');                
-            }
-        });
-    }
-
-
-
-    function validate(silent) {
-        var res = true;
-        var valid;
-        for(var i=0; i<cells.length; i++) {
-            for(var j=0; j<cells[i].length; j++) {
-                if(i >= params.valid.length || j > params.valid[i].length) {
-                    valid = false;
-                } else if(params.values[i][j] !== params.placeholder) {
-                    continue;
-                } else {
-                    var v1 = params.valid[i][j];
-                    var v2 = cells[i][j].text();
-                    valid = v1 !== '' && v1 === v2;
-                }
-                cells[i][j].toggleClass('mistake', !valid);
-                res = res && valid;
-            }
-        }
-        return res;
-    }
-
-
-
     function getAnswer() {
         var res = [];
-        for(var i=0; i<params.values.length; i++) {
-            var row = [];
-            for(var j=0; j<params.values[i].length; j++) {
-                row.push(params.values[i][j] === params.placeholder ? cells[i][j].text() : '');
-            }
-            res[i] = row;
+        for(var i=0; i<tables.length; i++) {
+            res.push(tables[i].getAnswer());
         }
-        return res;
     }
 
 
     function setAnswer(answer) {
-        reset();
-        var values = toolbar.find('.value').toArray();
-        for(var i=0; i<params.values.length; i++) {
-            for(var j=0; j<params.values[i].length; j++) {
-                if(params.values[i][j] !== params.placeholder) {
-                    continue;
-                }
-                for(var k=0; k<values.length; k++) {
-                    if(values[k].innerText === answer[i][j]) {
-                        cells[i][j].append(values[k]);
-                        values.splice(k, 1);
-                        break;
-                    }
-                }
-            }
+        for(var i=0; i<answer.length; i++) {
+            tables[i].resetValues();
+        }        
+        for(var i=0; i<answer.length; i++) {
+            tables[i].setAnswer(answer[i]);
         }
     }
 
 
-    function reset() {
-        table.find('td').removeClass('mistake');
-        toolbar.append(table.find('.value'));
+    function validate(silent) {
+        var res = true;
+        for(var i=0; i<tables.length; i++) {
+            if(!tables[i].validate(silent)) {
+                res = false;
+            }
+        }
+        return res;
     }
+
+
+
 
 
     return {
