e834a61e3821b9ff4cd3c2d93b088706a1ea2195
mobydimk <mobydimk@gmail.com>
2020-11-02 23:01:56 +0400

    json text editor update

--- pemFioi/components/json-text-editor/editor.js
+++ pemFioi/components/json-text-editor/editor.js
@@ -16,6 +16,8 @@ function JSONTextEditor(params) {
         tabSize: params.tab_size,
         useSoftTabs: true
     })
+    var Range = ace.require('ace/range').Range;
+    var mistake_marker;
 
     var on_change_timeout;
     editor.getSession().on('change', function(e) {
@@ -48,10 +50,32 @@ function JSONTextEditor(params) {
     }
 
 
+    function getSyntaxError(silent) {
+        if(mistake_marker) {
+            editor.getSession().removeMarker(mistake_marker);
+            mistake_marker = false;
+        }        
+        var annotations = editor.getSession().getAnnotations();
+        if(!annotations.length) {
+            return true;
+        }
+        var a = annotations[0];
+        if(!silent) {
+            editor.getSelection().setRange(new Range(0, 0, 0, 0)); // important string :)
+            var range = new Range(a.row, a.column, a.row, a.column + 1);                
+            mistake_marker = editor.getSession().addMarker(range, 'mistake', 'fullLine');            
+        }
+        return {
+            msg: a.text
+        }
+    }
+
+
     // interface
     return {
         setContent: setContent,
         getContent: getContent,
+        getSyntaxError: getSyntaxError,
         destroy: function() {
             clearTimeout(on_change_timeout);
             editor.destroy();
