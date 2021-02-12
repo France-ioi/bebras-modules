652cad3134d87e6e1e94ebeedec39d6303f72b0a
Arnaud Le Grand <arnaud.lgrd@gmail.com>
2021-02-12 15:30:46 +0100

    beav-1.0.js: add navigator functions

--- pemFioi/beav-1.0.js
+++ pemFioi/beav-1.0.js
@@ -328,6 +328,36 @@ Beav.Navigator.isIE8 = function() {
   return navigator.appVersion.indexOf("MSIE 8.") != -1;
 }
 
+Beav.Navigator.getVersion = function(){
+   var ua= navigator.userAgent, tem, 
+   M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+   if(/trident/i.test(M[1])){
+      tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+      return 'IE '+(tem[1] || '');
+   }
+   if(M[1]=== 'Chrome'){
+      tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+      if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+   }
+   M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+   if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+   return M.join(' ');
+}
+
+Beav.Navigator.supportsResponsiveMode = function() {
+   if(Beav.Navigator.isIE8){
+      return false
+   }
+   var navVersion = Beav.Navigator.getVersion();
+   if(navVersion.indexOf('MSIE') != -1){
+      return false
+   }
+   if(navVersion.indexOf('Firefox') != -1){
+      
+   }
+   return true
+}
+
 
 /**********************************************************************************/
 /* Dom */
