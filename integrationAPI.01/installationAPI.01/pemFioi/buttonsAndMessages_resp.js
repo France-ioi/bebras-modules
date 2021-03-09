e2442285b111c6f6c6cbadbef56bcaeb2fa6745b
Arnaud Le Grand <arnaud.lgrd@gmail.com>
2021-03-09 10:39:45 +0100

    buttonsAndMessages_resp.js: remove const for old browsers compatibility

--- integrationAPI.01/installationAPI.01/pemFioi/buttonsAndMessages_resp.js
+++ integrationAPI.01/installationAPI.01/pemFioi/buttonsAndMessages_resp.js
@@ -1184,7 +1184,7 @@ window.displayHelper = {
       if (!this.bUseFullWidth) {
          return
       }
-      const layout1Breakpoint = this.taskW / 0.6;
+      var layout1Breakpoint = this.taskW / 0.6;
       if(!this.responsive){
          $('#valider').appendTo($('#displayHelper_validate'));
          if(window.innerWidth >= 1200) {
@@ -1260,7 +1260,7 @@ window.displayHelper = {
 
    getScrollbarWidth: function () {
       // Creating invisible container
-      const outer = document.createElement('div');
+      var outer = document.createElement('div');
       outer.style.width = '200px';
       outer.style.visibility = 'hidden';
       outer.style.overflow = 'scroll'; // forcing scrollbar to appear
@@ -1268,11 +1268,11 @@ window.displayHelper = {
       document.body.appendChild(outer);
 
       // Creating inner element and placing it in the container
-      const inner = document.createElement('div');
+      var inner = document.createElement('div');
       outer.appendChild(inner);
 
       // Calculating difference between container's full width and the child width
-      const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
+      var scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
 
       // Removing temporary elements from the DOM
       outer.parentNode.removeChild(outer);
