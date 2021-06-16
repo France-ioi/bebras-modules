48b98e66f87b7c744e5bf8fd953b2d927230d12e
Pablo Mendoza <jpablo@gmail.com>
2021-06-16 16:55:08 -0500

    quickPi: Delay loading weather stuff until it's needed

--- ext/quickpi/quickpi.js
+++ ext/quickpi/quickpi.js
@@ -2439,6 +2439,7 @@ def gyroThread():
 # Begin getTemperatureFromCloud
    
 getTemperatureCloudUrl = "https://cloud.quick-pi.org/cache/weather.php"
+getTemperatureSupportedTowns = None
 
 def _getTemperatureSupportedTowns():
     import requests
@@ -2446,13 +2447,15 @@ def _getTemperatureSupportedTowns():
 
     return json.loads(requests.get(getTemperatureCloudUrl + "?q=supportedtowns").text)
 
-getTemperatureSupportedTowns = _getTemperatureSupportedTowns()
-
 getTemperatureCache = {}
 
 def getTemperatureFromCloud(town):
     import requests
-    import time
+    global getTemperatureSupportedTowns
+
+    if getTemperatureSupportedTowns is None:
+        getTemperatureSupportedTowns = _getTemperatureSupportedTowns()
+    
     current_milli_time = lambda: int(round(time.time() * 1000))
 
     if not town in getTemperatureSupportedTowns:
