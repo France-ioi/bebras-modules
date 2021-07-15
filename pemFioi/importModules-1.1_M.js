6662b23ae33be7f19b7aaa82f4d3d178d7224ac3
Arnaud Le Grand <arnaud.lgrd@gmail.com>
2021-07-15 11:33:41 +0200

    add gauge-1.0.js

--- pemFioi/importModules-1.1_M.js
+++ pemFioi/importModules-1.1_M.js
@@ -36,6 +36,7 @@ var importableModules = function () {
       'simpleKeyboard-1.0': {src: modulesPath+"/pemFioi/simpleKeyboard-1.0.js", id: "http://www.france-ioi.org/modules/pemFioi/simpleKeyboard-1.0.js"},
       'save-svg-as-png': {src: modulesPath+"/ext/save-svg-as-png/saveSvgAsPng.js", id: "save-svg-as-png"},
       'shape-paths': {src: modulesPath+"/pemFioi/shape-paths.js", id: "shape-paths"},
+      'gauge-1.0': {src: modulesPath+"/pemFioi/gauge-1.0.js", id: "gauge-1.0"},
 
       'platform-pr': {classStr: "proxy module", src: modulesPath+"/integrationAPI.01/official/platform-pr.js", id: "http://www.france-ioi.org/modules/integrationAPI.01/official/platform-pr.js"},
       'buttonsAndMessages': {classStr: "stdButtonsAndMessages module", src: modulesPath+"/integrationAPI.01/installationAPI.01/pemFioi/buttonsAndMessages.js",  id: "http://www.france-ioi.org/modules/integrationAPI.01/installationAPI.01/pemFioi/buttonsAndMessages.js"},
