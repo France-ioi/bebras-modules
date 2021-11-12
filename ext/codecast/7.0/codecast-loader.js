5de526ba69b3e721f1dc9619c4aecd8b9284152f
Sébastien Tainon <arkandias1@hotmail.fr>
2021-11-12 17:40:05 +0100

    Extract instructions from HTML to use them into Codecast

--- ext/codecast/7.0/codecast-loader.js
+++ ext/codecast/7.0/codecast-loader.js
@@ -1,13 +1,26 @@
 $(document).ready(function() {
   if (window.taskData) {
-    Codecast.start({
-      "start": "task",
-      "showStepper": true,
-      "showStack": true,
-      "showViews": true,
-      "showIO": true,
-      "platform": "python",
-      "task": window.taskData,
-    });
+    var taskInstructionsHtml = $('#taskIntro').html();
+    var additionnalOptions = window.taskData.codecastParameters ? window.taskData.codecastParameters : {};
+
+    var codecastParameters = $.extend(true, {
+      start: 'task',
+      showStepper: true,
+      showStack: true,
+      showViews: true,
+      showIO: true,
+      showDocumentation: true,
+      showFullScreen: true,
+      showMenu: true,
+      canRecord: false,
+      platform: 'python',
+      canChangePlatform: true,
+      canChangeLanguage: true,
+      controls: {},
+      task: window.taskData,
+      taskInstructions: taskInstructionsHtml,
+    }, additionnalOptions);
+
+    Codecast.start(codecastParameters);
   }
 });
