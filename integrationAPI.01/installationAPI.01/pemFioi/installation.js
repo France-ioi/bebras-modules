0098d4ef9e3a765b4e904f0fdee51c324dc42ccb
Michel Blockelet <blockelet@crans.org>
2022-01-19 16:50:23 +0100

    installation: Add mp3

--- integrationAPI.01/installationAPI.01/pemFioi/installation.js
+++ integrationAPI.01/installationAPI.01/pemFioi/installation.js
@@ -222,7 +222,7 @@ $(document).ready(function() {
 
 
 function fillImages(text, images, res) {
-   var extensions = ["png", "jpg", "gif", "ttf", "woff", "eot", "mp4", "zip"];
+   var extensions = ["png", "jpg", "gif", "ttf", "woff", "eot", "mp4", "zip", "mp3"];
    for (var iExt = 0; iExt < extensions.length; iExt++) {
       var ext = extensions[iExt];
       var regexp = new RegExp("[\'\"]([^;\"\']*." + ext + ")[\'\"]", "g");
