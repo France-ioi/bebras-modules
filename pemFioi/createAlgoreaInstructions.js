4479525f5d4030366d2975d5d686ad875eff3d92
Arnaud LG <arnaud.lgrd@gmail.com>
2022-01-24 15:59:21 +0100

    createAlgoreaInstructions.js: add dominoes

--- pemFioi/createAlgoreaInstructions.js
+++ pemFioi/createAlgoreaInstructions.js
@@ -1,22 +1,35 @@
-function createAlgoreaInstructions(type,params) {
+function createAlgoreaInstructions(type,level,params) {
    var languages = ["blockly","scratch","python"];
 
    var imgPath = "../../../_common/modules/img/algorea/";
    var vidPath = "../../../_common/modules/vid/";
-   var totalHTML = (params.moreDetails) ? "<div class='short'>" : "";
+   var totalHTML = "<div class='"+level+"'>";
+   totalHTML += (params.moreDetails) ? "<div class='short'>" : "";
    // 0 : course
-   if(type == 0){
-      totalHTML += createCourseInstructions(params);
+   // 1 : dominoes
+   switch(type){
+      case 0:
+         totalHTML += createCourseInstructions(params);
+         break;
+      case 1:
+         totalHTML += createDominoesInstructions(params);
+         break
+      default:
+         totalHTML += params.custom;
    }
+
+   for(var iLang = 0; iLang < languages.length; iLang++){
+      var lang = languages[iLang];
+      totalHTML += "<p data-lang="+lang+">Contenu "+lang+"</p>";  // test
+   }
+   
    totalHTML += (params.moreDetails) ? "</div>" : "";
 
    if(params.moreDetails){
       totalHTML += addDetails(params.moreDetails);
    }
-   for(var iLang = 0; iLang < languages.length; iLang++){
-      var lang = languages[iLang];
-      totalHTML += "<p data-lang="+lang+">Contenu "+lang+"</p>";  // test
-   }
+   
+   totalHTML += "</div>";
 
    return totalHTML
 
@@ -84,19 +97,67 @@ function createAlgoreaInstructions(type,params) {
       return html
    };
 
+   function createDominoesInstructions(params) {
+      // Programmez le robot pour qu'il ramasse le domino avec deux carrés bleus. 6
+      // Programmez le robot pour qu'il ramasse tous les dominos avec deux carrés bleus. 16
+      // Programmez le robot pour qu'il ramasse tous les dominos. 2
+      // Programmez le robot pour qu'il ramasse tous les dominos qui contiennent au moins un carré bleu. 1
+      // Programmez le robot pour qu'il ramasse tous les dominos qui ne contiennent pas de carré bleu. 1
+      // Programmez le robot pour qu'il ramasse tous les dominos qui ne contiennent pas de croix verte mais au moins un carré bleu. 1
+      // Programmez le robot pour qu'il ramasse tous les dominos sauf ceux qui contiennent deux carrés bleus. 1
+      // Programmez le robot pour qu'il ramasse tous les dominos avec deux carrés bleus, et seulement ceux-là. 7
+      // Programmez le robot pour qu'il ramasse le domino avec deux carrés bleus s'il y en a un, et seulement celui-là. 1
+      // Programmez le robot pour qu'il ramasse tous les dominos qui contiennent un carré ou une croix, et uniquement ceux là. 1
+      // Programmez le robot pour qu'il ramasse tous les dominos qui contiennent un carré ou une croix du côté gauche, et uniquement ceux là. 3
+      // Programmez le robot pour qu'il ramasse tous les dominos qui ont à la fois un carré bleu et une étoile orange. 1
+      // Programmez le robot pour qu'il ramasse tous les dominos qui contiennent au moins une croix verte et tous les dominos qui contiennent au moins une étoile orange. 1
+      // Programmez le robot pour qu'il ramasse tous les dominos qui contiennent deux étoiles orange. 1
+      // Programmez le robot pour qu'il ramasse tous les dominos qui contiennent deux motifs différents. 1
+
+      // Le robot peut passer sur les dominos qu'il ne ramasse pas. 8
+      // Le robot ne doit pas se déplacer plus de X fois. 4 
+      // Votre programme ne doit utiliser qu'une seule fois l'instruction ramasserDomino. 4
+      var strArr1 = [
+         "le domino avec deux carrés bleus.",
+         "le domino avec deux carrés bleus s'il y en a un, et seulement celui-là.",
+         "tous les dominos avec deux carrés bleus.",
+         "tous les dominos avec deux carrés bleus, et seulement ceux-là.",
+         "tous les dominos."
+      ];
+      var strID2 = [
+         "Le robot peut passer sur les dominos qu'il ne ramasse pas.",
+         "Votre programme ne doit utiliser qu'une seule fois l'instruction ramasserDomino.",
+         function(max) {
+            return "Le robot ne doit pas se déplacer plus de "+max+" fois."
+         }
+      ];
+      var strID1 = params.strID1;
+      var strID2 = params.strID2;
+      var html = "<p>"; 
+      html += "Programmez le robot pour qu'il ramasse ";
+      html += strArr1[strID1];
+      html += "</p>";
+      if(strID2 != undefined){
+         html += "<p>";
+         html += strArr2[strID2];
+         html += "</p>";
+      }
+      return html
+   };
+
    function addDetails(dat) {
       var html = "<div class='long";
-      if(dat.level){
-         for(var iLev = 0; iLev < dat.level.length; iLev++){
-            html += " "+dat.level[iLev];
-         }
-      }
-      html += "'";
+      // if(dat.level){
+      //    for(var iLev = 0; iLev < dat.level.length; iLev++){
+      //       html += " "+dat.level[iLev];
+      //    }
+      // }
+      // html += "'";
 
       if(dat.lang){
          html += " data-lang='";
          for(var iLang = 0; iLang < dat.lang.length; iLang++){
-            html += " "+dat.lang[iLev];
+            html += " "+dat.lang[iLang];
          }
          html += "'";
       }
@@ -132,5 +193,9 @@ function createAlgoreaInstructions(type,params) {
    // Si vous ne connaissez pas ce bloc, cliquez sur "Plus de détails" pour une introduction. 
    // Pour vous aider à comprendre vos erreurs, pensez au mode "Pas à Pas" : 
    // Vous ne pouvez utiliser que X instructions Y. 
+
+   // Si besoin, vous pouvez placer plusieurs blocs à l’intérieur du bloc “répéter”. 
+   // Si vous êtes bloqué, en particulier si vous n'avez plus assez de blocs pour finir votre programme, cliquez sur le bouton "Plus de détails" pour obtenir plus d'aide. 
+   // Dans cette version, les blocs sont regroupés par catégorie dans des menus. Cliquez sur un menu pour accéder aux blocs de la catégorie correspondante. 
    
 };
