951d8b1edbecc0ab69d0be11b36d5f926517dee9
Arnaud LG <arnaud.lgrd@gmail.com>
2022-02-03 10:12:39 +0100

    algorea : add gems img

--- pemFioi/blocklyRobot_lib-1.1.js
+++ pemFioi/blocklyRobot_lib-1.1.js
@@ -2129,11 +2129,12 @@ var getContext = function(display, infos, curLevel) {
          checkEndCondition: robotEndConditions.checkContainersFilled
       },
       gems: {
-         backgroundColor: "#e6b5d3",
+         backgroundColor: "#BF5E47",
+         borderColor: "#96413B",
          itemTypes: {
-            green_robot: { img: "green_robot.png", side: 80, nbStates: 9, isRobot: true, offsetX: -11, zOrder: 2 },
-            gem: { num: 3, img: "gem.png", side: 60, isWithdrawable: true, autoWithdraw: true, zOrder: 1 },
-            obstacle: { num: 4, img: "obstacle.png", side: 60, isObstacle: true, zOrder: 0 },
+            yellow_robot: { img: imgPath+"yellow_robot.png", side: 80, nbStates: 9, isRobot: true, offsetX: -11, zOrder: 2 },
+            gem: { num: 3, img: imgPath+"gem.png", side: 60, isWithdrawable: true, autoWithdraw: true, zOrder: 1 },
+            obstacle: { num: 4, img: imgPath+"brick_wall.png", side: 60, isObstacle: true, zOrder: 0 },
             number: { num: 5, side: 60, zOrder: 1 }
          },
          checkEndCondition: robotEndConditions.checkPickedAllWithdrawables
