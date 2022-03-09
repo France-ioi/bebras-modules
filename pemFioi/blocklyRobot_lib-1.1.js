bf1d56aa49f1264bb7b25923228aec59b6563899
Arnaud LG <arnaud.lgrd@gmail.com>
2022-03-09 21:44:05 +0100

    algorea: fix rocket background

--- pemFioi/blocklyRobot_lib-1.1.js
+++ pemFioi/blocklyRobot_lib-1.1.js
@@ -2379,7 +2379,7 @@ var getContext = function(display, infos, curLevel) {
          backgroundColor: "#2c293e",
          itemTypes: {
             robot: { img: imgPath+"white_robot.png", side: 80, nbStates: 9, isRobot: true, offsetX: -11, zOrder: 2 },
-            board_background: { num: 2, color: "#8d8dbe", side: 60, zOrder: 0},
+            board_background: { num: 2, color: "#2c293e", side: 60, zOrder: 0},
             stars: { num: 3, img: imgPath+"stars.png", side: 60, zOrder: 1},
             asteroide: { num: 4, img: imgPath+"asteroide.png", side: 60, isObstacle: true, zOrder: 1 },
             rocket: { num: 5, img: imgPath+"rocket.png", side: 60, isExit: true, zOrder: 1 },
