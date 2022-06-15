98b30e66dc31d6894f1df6d33ef9444ae7d8b6d1
Sébastien Tainon <arkandias1@hotmail.fr>
2022-06-15 10:56:02 +0200

    Add Codecast 7.1 for crane lib

---
+++ ext/codecast/7.1/codecast-loader.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  if (window.taskData) {
+    var taskInstructionsHtml = $('#taskIntro').html();
+    var additionalOptions = window.taskData.codecastParameters ? window.taskData.codecastParameters : {};
+
+    var codecastParameters = $.extend(true, {
+      start: 'task',
+      showStepper: true,
+      showStack: true,
+      showViews: true,
+      showIO: true,
+      showDocumentation: true,
+      showFullScreen: true,
+      showMenu: true,
+      canRecord: false,
+      platform: 'python',
+      canChangePlatform: true,
+      canChangeLanguage: true,
+      controls: {},
+      audioWorkerUrl: modulesPath + "ext/codecast/7.0/index.worker.worker.js",
+      baseUrl: "https://codecast.france-ioi.org/v7",
+      authProviders: ["algorea", "guest"],
+      task: window.taskData,
+      taskInstructions: taskInstructionsHtml,
+    }, additionalOptions);
+
+    Codecast.start(codecastParameters);
+  }
+});
--- pemFioi/importModules-1.5.js
+++ pemFioi/importModules-1.5.js
@@ -126,6 +126,9 @@
          'codecast7.0_css': {type: "stylesheet", src: modulesPath+"/ext/codecast/7.0/index.css", id: "codecast7.0_css"},
          'codecast7.0_js': {src: modulesPath+"/ext/codecast/7.0/index.js", id: "codecast7.0_js"},
          'codecast7.0_loader': {src: modulesPath+"/ext/codecast/7.0/codecast-loader.js", id: "codecast7.0_loader"},
+         'codecast7.1_css': {type: "stylesheet", src: modulesPath+"/ext/codecast/7.1/index.css", id: "codecast7.1_css"},
+         'codecast7.1_js': {src: modulesPath+"/ext/codecast/7.1/index.js", id: "codecast7.1_js"},
+         'codecast7.1_loader': {src: modulesPath+"/ext/codecast/7.1/codecast-loader.js", id: "codecast7.1_loader"},
 
          'blockly_database': {src: modulesPath+"/pemFioi/database/blockly_database.js", id: "blockly_database"},
          'database': {src: modulesPath+"/pemFioi/database/database.js", id: "database"},
@@ -259,6 +262,7 @@
          {name: 'scratch-base', included: ['scratch', 'scratch_blocks_common', 'scratch_blocks', 'blockly_javascript', 'blockly_python']},
          {name: 'python-analysis', languages: ['python'], included: ['react', 'react_dom', 'immutable', 'codecast_analysis', 'codecast_python_stack_view_container', 'codecast_python_function_header', 'codecast_python_function_locals', 'codecast_python_function_view', 'codecast_python_stack_view', 'codecast_python_variable', 'codecast_python_variable_value', 'codecast_css']},
          {name: 'codecast-7.0', included: ['codecast7.0_css', 'codecast7.0_js', 'codecast7.0_loader']}
+         {name: 'codecast-7.1', included: ['codecast7.1_css', 'codecast7.1_js', 'codecast7.1_loader']}
 // TODO :: bundles with mobileFirst interface
 //      {name: 'quickAlgo-all-blockly', included: ['quickAlgo_utils', 'quickAlgo_i18n', 'quickAlgo_interface', 'quickAlgo_blockly_blocks','quickAlgo_blockly_interface', 'quickAlgo_blockly_runner', 'quickAlgo_subtask', 'quickAlgo_context']},
 //      {name: 'quickAlgo-all-python', included: ['python_count', 'ace', 'ace_python', 'skulpt_quickAlgo', 'skulpt_stdlib', 'skulpt_debugger', 'quickAlgo_utils', 'quickAlgo_i18n', 'quickAlgo_interface', 'quickAlgo_python_interface', 'quickAlgo_python_runner', 'quickAlgo_subtask', 'quickAlgo_context']}
