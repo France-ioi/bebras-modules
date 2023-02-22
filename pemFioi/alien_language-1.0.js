6ec9148b0c984b513d31511e3733eb40423a754e
Sébastien Tainon <arkandias1@hotmail.fr>
2023-02-22 11:38:21 +0100

    Do not redecline fixed attr when generating dictionary

--- pemFioi/alien_language-1.0.js
+++ pemFioi/alien_language-1.0.js
@@ -1566,9 +1566,9 @@ function generateDictionary() {
          let stem = word.stem;
          let fixedAttrVal = word.fixedAttrVal;
          let currAttrValues = {};
-         for (let attrID in fixedAttrVal) {
-            currAttrValues[attrID] = fixedAttrVal[attrID];
-         }
+         // for (let attrID in fixedAttrVal) {
+         //    currAttrValues[attrID] = fixedAttrVal[attrID];
+         // }
          let varAttr = gramTypeData[gramTypeID].attributes.variable;
 
          let allVarAttrPossibilites = [];
