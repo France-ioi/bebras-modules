406265bd40ea1505d063281767cf84df46f88eba
Sébastien Tainon <5164914+SebastienTainon@users.noreply.github.com>
2023-04-05 10:25:08 +0200

    Add Codecast 7.3 (#105)

---
+++ ext/codecast/7.3/codecast-loader.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+  if (window.taskData) {
+    var taskInstructionsHtml = $('#taskIntro').html();
+
+    var hints = $('#taskHints > div').toArray().map(elm => {
+      return {
+        content: elm.innerHTML.trim()
+      };
+    });
+
+    var additionalOptions = window.taskData.codecastParameters ? window.taskData.codecastParameters : {};
+    if (window.codecastPreload) {
+      additionalOptions.preload = true;
+    }
+    if (!additionalOptions.language && window.stringsLanguage) {
+      additionalOptions.language = window.stringsLanguage;
+    }
+
+    var codecastParameters = $.extend(true, {
+      start: 'task',
+      showStepper: true,
+      showStack: true,
+      showViews: true,
+      showIO: true,
+      showDocumentation: true,
+      showFullScreen: true,
+      showMenu: true,
+      canRecord: false,
+      platform: 'python',
+      canChangePlatform: true,
+      canChangeLanguage: true,
+      controls: {},
+      audioWorkerUrl: window.modulesPath + "ext/codecast/7.2/index.worker.worker.js",
+      baseUrl: "https://codecast.france-ioi.org/v7",
+      authProviders: ["algorea", "guest"],
+      task: window.taskData,
+      taskInstructions: taskInstructionsHtml,
+      taskHints: hints,
+    }, additionalOptions, window.codecastPreload ? window.codecastPreload : {});
+
+    Codecast.start(codecastParameters);
+  }
+});
