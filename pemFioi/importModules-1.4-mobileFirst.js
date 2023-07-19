5389e619492108d7eedf95b9331f6e1e3c076cf0
Sébastien Tainon <5164914+SebastienTainon@users.noreply.github.com>
2023-07-19 15:02:13 +0200

    Import recursively bundles in importModules() (#112)

--- pemFioi/importModules-1.4-mobileFirst.js
+++ pemFioi/importModules-1.4-mobileFirst.js
@@ -332,7 +332,8 @@
          {name: 'codecast-7.0', included: ['codecast7.0_css', 'codecast7.0_js', 'codecast7.0_loader']},
          {name: 'codecast-7.1', included: ['codecast7.1_css', 'codecast7.1_js', 'codecast7.1_loader']},
          {name: 'codecast-7.2', included: ['codecast7.2_css', 'codecast7.2_js', 'codecast7.2_loader']},
-         {name: 'codecast-7.3', included: ['codecast7.3_css', 'codecast7.3_js', 'codecast7.3_loader']}
+         {name: 'codecast-7.3', included: ['codecast7.3_css', 'codecast7.3_js', 'codecast7.3_loader']},
+         {name: 'smart-contract', included: ['jquery-1.7.1', 'JSON-js', 'raphael-2.2.1', 'codecast-7.3', 'jschannel', 'raphaelFactory-1.0', 'delayFactory-1.0', 'simulationFactory-1.0', 'platform-pr', 'beav-1.0', 'installationAPI.01', 'miniPlatform', 'conceptDisplay-1.0', 'conceptViewer-1.0', 'conceptViewer_css-1.0', 'taskStyles-mobileFirst']}
 // TODO :: bundles with mobileFirst interface
 //      {name: 'quickAlgo-all-blockly', included: ['quickAlgo_utils', 'quickAlgo_i18n', 'quickAlgo_interface', 'quickAlgo_blockly_blocks','quickAlgo_blockly_interface', 'quickAlgo_blockly_runner', 'quickAlgo_subtask', 'quickAlgo_context']},
 //      {name: 'quickAlgo-all-python', included: ['python_count', 'ace', 'ace_python', 'skulpt_quickAlgo', 'skulpt_stdlib', 'skulpt_debugger', 'quickAlgo_utils', 'quickAlgo_i18n', 'quickAlgo_interface', 'quickAlgo_python_interface', 'quickAlgo_python_runner', 'quickAlgo_subtask', 'quickAlgo_context']}
@@ -384,12 +385,20 @@
          newList.push(module);
          includedModules[module] = true;
       }
+      function addBundle(bundle) {
+         for(var iSub in bundlesByName[bundle].included) {
+            var moduleName = bundlesByName[bundle].included[iSub];
+            if (bundlesByName[moduleName]) {
+               addBundle(moduleName);
+            } else {
+               addModule(moduleName);
+            }
+         }
+      }
       for(var iModule in modulesList) {
          var curModule = modulesList[iModule];
          if(bundlesByName[curModule]) {
-            for(var iSub in bundlesByName[curModule].included) {
-               addModule(bundlesByName[curModule].included[iSub]);
-            }
+            addBundle(curModule);
          } else {
             addModule(curModule);
          }
