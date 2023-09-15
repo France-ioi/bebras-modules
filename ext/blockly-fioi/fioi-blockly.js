3dd3875c7f68e0cdd67aab36d39e0f52cdece765
Sébastien Tainon <arkandias1@hotmail.fr>
2023-09-15 11:45:52 +0200

    Upgrade Codecast 7.3 and fioi blockly

--- ext/blockly-fioi/fioi-blockly.js
+++ ext/blockly-fioi/fioi-blockly.js
@@ -1880,101 +1880,82 @@ FioiBlockly.Msg.it.TABLES_OUT_OF_BOUNDS = "Tentativo d'accesso a una casella fuo
 FioiBlockly.Msg.it.MATH_DIVISIONFLOOR_SYMBOL = ' // ';
 FioiBlockly.Msg.it.MATH_ARITHMETIC_TOOLTIP_DIVIDEFLOOR = "Riporta la parte intera della divisione dei due numeri.";
 
-// Temporarily put fr translations, waiting the NL ones
-
 FioiBlockly.Msg.nl = {};
-
-FioiBlockly.Msg.nl.VARIABLES_DEFAULT_NAME = "element";
-FioiBlockly.Msg.nl.TEXT_APPEND_VARIABLE = Blockly.Msg.VARIABLES_DEFAULT_NAME;
-
-FioiBlockly.Msg.nl.DICTS_CREATE_EMPTY_TITLE = "dictionnaire vide";
-FioiBlockly.Msg.nl.DICTS_CREATE_WITH_CONTAINER_TITLE_ADD = "Créer un dictionnaire";
-FioiBlockly.Msg.nl.DICTS_CREATE_WITH_CONTAINER_TOOLTIP = "";
-FioiBlockly.Msg.nl.DICTS_CREATE_WITH_INPUT_WITH = "créer un dictionnaire :";
-FioiBlockly.Msg.nl.DICTS_CREATE_WITH_ITEM_KEY = "cle";
-FioiBlockly.Msg.nl.DICTS_CREATE_WITH_ITEM_MAPPING = ":";
-FioiBlockly.Msg.nl.DICTS_CREATE_WITH_ITEM_TITLE = "clé/valeur";
-FioiBlockly.Msg.nl.DICTS_CREATE_WITH_ITEM_TOOLTIP = "";
-FioiBlockly.Msg.nl.DICTS_CREATE_WITH_TOOLTIP = "";
-FioiBlockly.Msg.nl.DICT_GET = "récupérer la clé";
-FioiBlockly.Msg.nl.DICT_GET_TO = "de";
-FioiBlockly.Msg.nl.DICT_KEYS = "liste des clés de";
-FioiBlockly.Msg.nl.DICT_SET_TITLE = "affecter la clé";
-FioiBlockly.Msg.nl.DICT_SET_OF = "du dictionnaire";
-FioiBlockly.Msg.nl.DICT_SET_TO = "à";
-
-FioiBlockly.Msg.nl.TEXT_PRINT_TITLE = "afficher la ligne %1";
-FioiBlockly.Msg.nl.TEXT_PRINT_TOOLTIP = "Afficher le texte, le nombre ou une autre valeur spécifiée, avec retour à la ligne après.";
-FioiBlockly.Msg.nl.TEXT_PRINT_NOEND_TITLE = "afficher %1";
-FioiBlockly.Msg.nl.TEXT_PRINT_NOEND_TOOLTIP = "Afficher le texte, le nombre ou une autre valeur spécifiée, sans retour à la ligne.";
-
-FioiBlockly.Msg.nl.TEXT_EVAL_TITLE = "évaluer";
-FioiBlockly.Msg.nl.TEXT_EVAL_TOOLTIP = "Évalue l'expression arithmétique spécifiée.";
-FioiBlockly.Msg.nl.TEXT_EVAL_INVALID = "Attention : %1 ; ce bloc retournera 'faux' !";
-
-FioiBlockly.Msg.nl.EVAL_ERROR_SEMICOLON = "le point-virgule ';' n'est pas autorisé";
-FioiBlockly.Msg.nl.EVAL_ERROR_SYNTAX = "l'expression n'est pas syntaxiquement valide";
-FioiBlockly.Msg.nl.EVAL_ERROR_TYPE = "ce type d'expression (%1) n'est pas autorisé";
-FioiBlockly.Msg.nl.EVAL_ERROR_VAR = "cette expression utilise une variable '%1' non définie";
-
-FioiBlockly.Msg.nl.LISTS_APPEND_MSG = "à la liste %1 ajouter l'élément %2";
-FioiBlockly.Msg.nl.LISTS_APPEND_TOOLTIP = "Ajouter un élément à la liste '%1'";
-FioiBlockly.Msg.nl.LISTS_CREATE_WITH_TOO_LARGE = "Taille de la liste trop grande : %1 > taille maximale autorisée %2"
-FioiBlockly.Msg.nl.LISTS_GET_INDEX_FIRST = "au début";
-FioiBlockly.Msg.nl.LISTS_GET_INDEX_FROM_END = "à l'indice depuis la fin";
-FioiBlockly.Msg.nl.LISTS_GET_INDEX_FROM_START = "à l'indice";
-FioiBlockly.Msg.nl.LISTS_GET_INDEX_GET = "obtenir la valeur";
-FioiBlockly.Msg.nl.LISTS_GET_INDEX_GET_REMOVE = "obtenir et supprimer la valeur";
-FioiBlockly.Msg.nl.LISTS_GET_INDEX_LAST = "à la fin";
-FioiBlockly.Msg.nl.LISTS_GET_INDEX_RANDOM = "à un indice aléatoire";
-FioiBlockly.Msg.nl.LISTS_GET_INDEX_REMOVE = "supprimer la valeur";
-FioiBlockly.Msg.nl.LISTS_SET_INDEX_INSERT = "insérer";
-FioiBlockly.Msg.nl.LISTS_SORT_TITLE = "renvoyer le tri %1 %2 de la liste %3"
-FioiBlockly.Msg.nl.LISTS_SORT_PLACE_MSG = "trier la liste %1 sur place";
-FioiBlockly.Msg.nl.LISTS_SORT_PLACE_TOOLTIP = "Trie la liste '%1' et la modifie directement.";
-
-FioiBlockly.Msg.nl.INPUT_NUM = "lire un nombre seul sur une ligne";
-FioiBlockly.Msg.nl.INPUT_NUM_TOOLTIP = "Lit un nombre seul sur une ligne, sur l'entrée du programme.";
-FioiBlockly.Msg.nl.INPUT_NUM_LIST = "lire une liste de nombres sur une ligne";
-FioiBlockly.Msg.nl.INPUT_NUM_LIST_TOOLTIP = "Lit une liste de nombres sur une ligne, sur l'entrée du programme.";
-FioiBlockly.Msg.nl.INPUT_NUM_NEXT = "lire un nombre";
-FioiBlockly.Msg.nl.INPUT_NUM_NEXT_TOOLTIP = "Lit le prochain nombre sur l'entrée du programme.";
-FioiBlockly.Msg.nl.INPUT_CHAR = "lire un caractère";
-FioiBlockly.Msg.nl.INPUT_CHAR_TOOLTIP = "Lit un caractère sur l'entrée du programme.";
-FioiBlockly.Msg.nl.INPUT_WORD = "lire un mot";
-FioiBlockly.Msg.nl.INPUT_WORD_TOOLTIP = "Lit un mot sur l'entrée du programme.";
-FioiBlockly.Msg.nl.INPUT_LINE = "lire une ligne";
-FioiBlockly.Msg.nl.INPUT_LINE_TOOLTIP = "Lit une ligne sur l'entrée du programme.";
-
-FioiBlockly.Msg.nl.CANNOT_DELETE_VARIABLE_PROCEDURE = "Impossible de supprimer la variable '%1', utilisée par la procédure '%2'.";
-
-FioiBlockly.Msg.nl.DATA_REPLACEITEMOFLIST_TITLE = "remplacer l'élément %1 de la liste %2 par %3";
-FioiBlockly.Msg.nl.DATA_ITEMOFLIST_TITLE = "élément %1 dans %2";
-FioiBlockly.Msg.nl.DATA_LISTREPEAT_TITLE = "initialiser la liste %1 avec %2 répété %3 fois";
-
-FioiBlockly.Msg.nl.INVALID_NAME = "Nom invalide, veuillez n'utiliser que des lettres, lettres accentuées françaises, chiffres (sauf comme premier caractère) et tiret bas '_'.";
-
-FioiBlockly.Msg.nl.TABLES_2D_INIT = "initialiser le tableau 2D %1 avec %2 lignes et %3 colonnes contenant %4";
-FioiBlockly.Msg.nl.TABLES_2D_INIT_TOOLTIP = "Crée un tableau avec le nombre spécifié de lignes et de colonnes, et initialise chaque case à la valeur donnée.";
-FioiBlockly.Msg.nl.TABLES_2D_SET = "dans %1[%2] [%3] mettre %4";
-FioiBlockly.Msg.nl.TABLES_3D_SET_TOOLTIP = "Met la valeur dans la case [ligne] [colonne] du tableau %1.";
-FioiBlockly.Msg.nl.TABLES_2D_GET = "%1[%2] [%3]";
-FioiBlockly.Msg.nl.TABLES_2D_GET_TOOLTIP = "Récupère la valeur dans la case [ligne] [colonne] du tableau %1.";
-
-FioiBlockly.Msg.nl.TABLES_3D_INIT = "initialiser le tableau 3D %1 avec %2 couches, %3 lignes, %4 colonnes contenant %5";
-FioiBlockly.Msg.nl.TABLES_3D_INIT_TOOLTIP = "Crée un tableau avec le nombre spécifié de lignes, de colonnes et de niveaux, et initialise chaque case à la valeur donnée.";
-FioiBlockly.Msg.nl.TABLES_3D_SET = "dans %1[%2] [%3] [%4] mettre %5";
-FioiBlockly.Msg.nl.TABLES_3D_SET_TOOLTIP = "Met la valeur dans la case [couche] [ligne] [colonne] du tableau %1.";
-FioiBlockly.Msg.nl.TABLES_3D_GET = "%1[%2] [%3] [%4]";
-FioiBlockly.Msg.nl.TABLES_3D_GET_TOOLTIP = "Récupère la valeur dans la case [couche] [ligne] [colonne] du tableau %1.";
-
-FioiBlockly.Msg.nl.TABLES_VAR_NAME = "tableau";
-FioiBlockly.Msg.nl.TABLES_TOO_BIG = "Dimensions du tableau trop grandes !";
-FioiBlockly.Msg.nl.TABLES_OUT_OF_BOUNDS = "Tentative d'accès à une case hors du tableau !";
-
-FioiBlockly.Msg.nl.MATH_DIVISIONFLOOR_SYMBOL = ' // ';
-FioiBlockly.Msg.nl.MATH_ARITHMETIC_TOOLTIP_DIVIDEFLOOR = "Renvoie la partie entière de la division des deux nombres.";
-
+FioiBlockly.Msg.nl.VARIABLES_DEFAULT_NAME = 'element';
+FioiBlockly.Msg.nl.DICTS_CREATE_EMPTY_TITLE = 'leeg woordenboek';
+FioiBlockly.Msg.nl.DICTS_CREATE_WITH_CONTAINER_TITLE_ADD = 'Een woordenboek maken';
+FioiBlockly.Msg.nl.DICTS_CREATE_WITH_CONTAINER_TOOLTIP = '';
+FioiBlockly.Msg.nl.DICTS_CREATE_WITH_INPUT_WITH = 'Een woordenboek maken';
+FioiBlockly.Msg.nl.DICTS_CREATE_WITH_ITEM_KEY = 'sleutel';
+FioiBlockly.Msg.nl.DICTS_CREATE_WITH_ITEM_MAPPING = '';
+FioiBlockly.Msg.nl.DICTS_CREATE_WITH_ITEM_TITLE = 'sleutel/waarde';
+FioiBlockly.Msg.nl.DICTS_CREATE_WITH_ITEM_TOOLTIP = '';
+FioiBlockly.Msg.nl.DICTS_CREATE_WITH_TOOLTIP = '';
+FioiBlockly.Msg.nl.DICT_GET = 'de sleutel ophalen';
+FioiBlockly.Msg.nl.DICT_GET_TO = 'van';
+FioiBlockly.Msg.nl.DICT_KEYS = 'lijst van de sleutels van';
+FioiBlockly.Msg.nl.DICT_SET_TITLE = 'de sleutel toewijzen';
+FioiBlockly.Msg.nl.DICT_SET_OF = 'van het woordenboek';
+FioiBlockly.Msg.nl.DICT_SET_TO = 'aan';
+FioiBlockly.Msg.nl.TEXT_PRINT_TITLE = 'de regel weergeven %1';
+FioiBlockly.Msg.nl.TEXT_PRINT_TOOLTIP = 'De tekst, het getal of een andere opgegeven waarde weergeven, gevolgd door een regeleinde.';
+FioiBlockly.Msg.nl.TEXT_PRINT_NOEND_TITLE = 'weergeven %1';
+FioiBlockly.Msg.nl.TEXT_PRINT_NOEND_TOOLTIP = 'De tekst, het getal of een andere opgegeven waarde weergeven, zonder regeleinde.';
+FioiBlockly.Msg.nl.TEXT_EVAL_TITLE = 'evalueren';
+FioiBlockly.Msg.nl.TEXT_EVAL_TOOLTIP = 'Evalueert de opgegeven rekenkundige bewerking.';
+FioiBlockly.Msg.nl.TEXT_EVAL_INVALID = "Opgelet : %1 ; dit blok zal terugkeren 'fout' !";
+FioiBlockly.Msg.nl.EVAL_ERROR_SEMICOLON = "de puntkomma ';' is niet toegestaan";
+FioiBlockly.Msg.nl.EVAL_ERROR_SYNTAX = 'de uitdrukking is syntactisch niet geldig';
+FioiBlockly.Msg.nl.EVAL_ERROR_TYPE = 'dit type uitdrukking (%1) is niet toegestaan';
+FioiBlockly.Msg.nl.EVAL_ERROR_VAR = "deze uitdrukking gebruikt een ongedefinieerde '%1' variabele";
+FioiBlockly.Msg.nl.LISTS_APPEND_MSG = 'voeg element %2 toe aan lijst %1';
+FioiBlockly.Msg.nl.LISTS_APPEND_TOOLTIP = "Voeg een element toe aan de lijst '%1'";
+FioiBlockly.Msg.nl.LISTS_CREATE_WITH_TOO_LARGE = 'Lijstgrootte te groot: %1 > maximaal toegestane grootte %2';
+FioiBlockly.Msg.nl.LISTS_GET_INDEX_FIRST = 'in het begin';
+FioiBlockly.Msg.nl.LISTS_GET_INDEX_FROM_END = 'aan de index vanaf het einde ';
+FioiBlockly.Msg.nl.LISTS_GET_INDEX_FROM_START = 'aan de index  ';
+FioiBlockly.Msg.nl.LISTS_GET_INDEX_GET = 'de waarde bekomen';
+FioiBlockly.Msg.nl.LISTS_GET_INDEX_GET_REMOVE = 'de waarde bekomen en wissen';
+FioiBlockly.Msg.nl.LISTS_GET_INDEX_LAST = 'op het einde';
+FioiBlockly.Msg.nl.LISTS_GET_INDEX_RANDOM = 'naar een willekeurige hint';
+FioiBlockly.Msg.nl.LISTS_GET_INDEX_REMOVE = 'de waarde wissen';
+FioiBlockly.Msg.nl.LISTS_SET_INDEX_INSERT = 'invoegen';
+FioiBlockly.Msg.nl.LISTS_SORT_TITLE = 'stuur het gesorteerde terug %1 %2 van de lijst %3';
+FioiBlockly.Msg.nl.LISTS_SORT_PLACE_MSG = 'de lijst sorteren %1 ter plaatse';
+FioiBlockly.Msg.nl.LISTS_SORT_PLACE_TOOLTIP = "Sorteer de lijst ' %1' en wijzig ze onmiddellijk.";
+FioiBlockly.Msg.nl.INPUT_NUM = 'één enkel getal op één regel lezen';
+FioiBlockly.Msg.nl.INPUT_NUM_TOOLTIP = 'Leest één enkel getal op één regel, op de programma-invoer.';
+FioiBlockly.Msg.nl.INPUT_NUM_LIST = 'een lijst met getallen op één regel lezen';
+FioiBlockly.Msg.nl.INPUT_NUM_LIST_TOOLTIP = 'Leest een lijst getallen op één regel, uit de programma-invoer.';
+FioiBlockly.Msg.nl.INPUT_NUM_NEXT = 'een getal lezen';
+FioiBlockly.Msg.nl.INPUT_NUM_NEXT_TOOLTIP = 'Leest het volgende getal op de programma-invoer.';
+FioiBlockly.Msg.nl.INPUT_CHAR = 'Een karakter lezen';
+FioiBlockly.Msg.nl.INPUT_CHAR_TOOLTIP = 'Leest een karakter op de programma-invoer.';
+FioiBlockly.Msg.nl.INPUT_WORD = 'een woord lezen';
+FioiBlockly.Msg.nl.INPUT_WORD_TOOLTIP = 'Leest een woord op de programma-invoer.';
+FioiBlockly.Msg.nl.INPUT_LINE = 'een regel lezen';
+FioiBlockly.Msg.nl.INPUT_LINE_TOOLTIP = 'Leest een regel op de programma-invoer.';
+FioiBlockly.Msg.nl.CANNOT_DELETE_VARIABLE_PROCEDURE = "Variabele '%1', gebruikt door procedure '%2', kan niet worden verwijderd.";
+FioiBlockly.Msg.nl.DATA_REPLACEITEMOFLIST_TITLE = 'vervang het element %1 in lijst %2 door %3';
+FioiBlockly.Msg.nl.DATA_ITEMOFLIST_TITLE = 'element %1 in %2';
+FioiBlockly.Msg.nl.DATA_LISTREPEAT_TITLE = 'initialiseer lijst %1 met %2 herhaald %3 keer';
+FioiBlockly.Msg.nl.INVALID_NAME = "Ongeldige naam, gebruik alleen letters, letters met Franse accenten, cijfers (behalve als eerste teken) en underscore '_'.";
+FioiBlockly.Msg.nl.TABLES_2D_INIT = 'initialiseer de 2D-tabel %1 met %2 rijen en %3 kolommen die %4 bevatten';
+FioiBlockly.Msg.nl.TABLES_2D_INIT_TOOLTIP = 'Creëer een tabel met het opgegeven aantal rijen en kolommen en initialiseer elk veld met de opgegeven waarde.';
+FioiBlockly.Msg.nl.TABLES_2D_SET = 'in %1[%2] [%3] zet %4';
+FioiBlockly.Msg.nl.TABLES_3D_SET_TOOLTIP = 'Zet de waarde in het veld [laag] [rij] [kolom] in de tabel %1.';
+FioiBlockly.Msg.nl.TABLES_2D_GET = '%1[%2] [%3]';
+FioiBlockly.Msg.nl.TABLES_2D_GET_TOOLTIP = 'Haal de waarde op uit het veld [rij] [kolom] in de tabel %1.';
+FioiBlockly.Msg.nl.TABLES_3D_INIT = 'initialiseer de 3D-tabel %1 met %2 lagen, %3 rijen, %4 kolommen met %5';
+FioiBlockly.Msg.nl.TABLES_3D_INIT_TOOLTIP = 'Creëer een tabel met het opgegeven aantal rijen, kolommen en niveaus en initialiseer elk veld met de opgegeven waarde.';
+FioiBlockly.Msg.nl.TABLES_3D_SET = 'in %1[%2] [%3] [%4] zet %5';
+FioiBlockly.Msg.nl.TABLES_3D_GET = '%1[%2] [%3] [%4]';
+FioiBlockly.Msg.nl.TABLES_3D_GET_TOOLTIP = 'Haal de waarde op uit het vak [laag] [rij] [kolom] in de tabel %1.';
+FioiBlockly.Msg.nl.TABLES_VAR_NAME = 'tabel';
+FioiBlockly.Msg.nl.TABLES_TOO_BIG = 'Afmetingen van de tabel te groot!';
+FioiBlockly.Msg.nl.TABLES_OUT_OF_BOUNDS = 'Poging tot toegang tot een veld buiten de tabel!';
+FioiBlockly.Msg.nl.MATH_DIVISIONFLOOR_SYMBOL = '//';
+FioiBlockly.Msg.nl.MATH_ARITHMETIC_TOOLTIP_DIVIDEFLOOR = 'Geeft het gehele getal van de deling van de twee getallen.';
 FioiBlockly.Msg.sl = {};
 
 FioiBlockly.Msg.sl.VARIABLES_DEFAULT_NAME = "element";
