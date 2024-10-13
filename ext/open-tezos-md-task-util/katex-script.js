d152f662a4dff9742b09c0f97b22960cc9fd7016
Michel Blockelet <blockelet@crans.org>
2024-10-13 13:14:19 +0000

    katex: don't render twice

--- ext/open-tezos-md-task-util/katex-script.js
+++ ext/open-tezos-md-task-util/katex-script.js
@@ -3,12 +3,16 @@ if (!window.instructionsPostProcessing) {
 }
 
 window.instructionsPostProcessing.push(function () {
-   const inlineMaths = document.querySelectorAll('span.math.math-inline');
-   inlineMaths.forEach(span => {
-     try {
-       katex.render(span.innerText, span);
-     } catch (err) {
-       console.error('Error rendering KaTeX:', err);
-     }
-   });
- });
+  const inlineMaths = document.querySelectorAll('span.math.math-inline');
+  inlineMaths.forEach(span => {
+    if (span.querySelectorAll('span.katex').length > 0) {
+      // Avoid rendering twice (KaTeX will not detect)
+      return;
+    }
+    try {
+      katex.render(span.innerText, span);
+    } catch (err) {
+      console.error('Error rendering KaTeX:', err);
+    }
+  });
+});
