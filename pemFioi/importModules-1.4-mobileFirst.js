61dfa67d262b8d952ae1f3affe0b895302230385
Arnaud LG <arnaud.lgrd@gmail.com>
2025-03-27 16:14:02 +0100

    blocklyIA_lib-1.0: backup

--- pemFioi/importModules-1.4-mobileFirst.js
+++ pemFioi/importModules-1.4-mobileFirst.js
@@ -98,6 +98,7 @@
          'blockly-turtle': {src: modulesPath+"/pemFioi/blocklyTurtle_lib.js", id: "blocklyTurtle_lib"},
          'blockly-processing': {src: modulesPath+"/pemFioi/blocklyProcessing_lib.js", id: "blocklyProcessing_lib"},
          'jwinf_css': {type: "stylesheet", src: modulesPath+"/pemFioi/jwinf.css", id: "jwinf_css"}, // for BWINF
+         'blockly-ia-1.0': {src: modulesPath+"/pemFioi/blocklyIA_lib-1.0.js", id: "blocklyIA_lib"},
 
          'quickAlgo_utils': {src: modulesPath+"/pemFioi/quickAlgo/utils.js", id: "quickAlgo_utils"},
          'quickAlgo_i18n': {src: modulesPath+"/pemFioi/quickAlgo/i18n.js", id: "quickAlgo_i18n"},
