86b7ac0337bc884ca71d564653a18d76cacac250
Michel Blockelet <blockelet@crans.org>
2025-04-09 07:43:29 +0000

    installation: Add pdf and csv files

--- integrationAPI.01/installationAPI.01/pemFioi/installation.js
+++ integrationAPI.01/installationAPI.01/pemFioi/installation.js
@@ -248,7 +248,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 function fillImages(text, images, res) {
-   var extensions = ["png", "jpg", "gif", "ttf", "woff", "eot", "mp4", "zip", "mp3"];
+   var extensions = ["png", "jpg", "gif", "ttf", "woff", "eot", "mp4", "zip", "mp3", "pdf", "csv"];
    for (var iExt = 0; iExt < extensions.length; iExt++) {
       var ext = extensions[iExt];
       var regexp = new RegExp("[\'\"]([^;\"\']*." + ext + ")[\'\"]", "g");
