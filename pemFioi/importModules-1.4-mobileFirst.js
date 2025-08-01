b3488d0dfd36dc42af256ed74c16ac3d81047f1a
Sébastien Tainon <arkandias1@hotmail.fr>
2025-08-01 11:38:22 +0200

    Update ai-tasks-tools modules

--- pemFioi/importModules-1.4-mobileFirst.js
+++ pemFioi/importModules-1.4-mobileFirst.js
@@ -358,7 +358,7 @@
          { name: 'smart-contract', included: ['jquery-1.7.1', 'JSON-js', 'raphael-2.2.1', 'codecast-7.4', 'jschannel', 'raphaelFactory-1.0', 'delayFactory-1.0', 'simulationFactory-1.0', 'platform-pr', 'beav-1.0', 'installationAPI.01', 'miniPlatform', 'conceptDisplay-1.0', 'conceptViewer-1.0', 'conceptViewer_css-1.0', 'taskStyles-mobileFirst'] },
          { name: 'server-task', included: ['jquery-1.7.1', 'JSON-js', 'raphael-2.2.1', 'codecast-7.5', 'jschannel', 'raphaelFactory-1.0', 'delayFactory-1.0', 'simulationFactory-1.0', 'platform-pr', 'beav-1.0', 'installationAPI.01', 'miniPlatform', 'conceptDisplay-1.0', 'conceptViewer-1.0', 'conceptViewer_css-1.0', 'taskStyles-mobileFirst'] },
          {name: 'connected-boards', included: ['connected-boards-js', 'connected-boards-css']},
-         {name: 'ai-tasks-tools', included: ['ai-tasks-tools-js', 'ai-tasks-tools-css']}
+         {name: 'ai-tasks-tools', included: ['platform-pr',  'jquery-1.7.1', 'jschannel', 'installationAPI.01', 'miniPlatform', 'ai-tasks-tools-js', 'ai-tasks-tools-css']}
 // TODO :: bundles with mobileFirst interface
 //      {name: 'quickAlgo-all-blockly', included: ['quickAlgo_utils', 'quickAlgo_i18n', 'quickAlgo_interface', 'quickAlgo_blockly_blocks','quickAlgo_blockly_interface', 'quickAlgo_blockly_runner', 'quickAlgo_subtask', 'quickAlgo_context']},
 //      {name: 'quickAlgo-all-python', included: ['python_count', 'ace', 'ace_python', 'skulpt_quickAlgo', 'skulpt_stdlib', 'skulpt_debugger', 'quickAlgo_utils', 'quickAlgo_i18n', 'quickAlgo_interface', 'quickAlgo_python_interface', 'quickAlgo_python_runner', 'quickAlgo_subtask', 'quickAlgo_context']}
