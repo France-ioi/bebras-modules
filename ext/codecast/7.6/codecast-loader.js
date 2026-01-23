0de88f0b740174e44146d81493e8dbe2d174901f
Sébastien Tainon <arkandias1@hotmail.fr>
2026-01-23 10:58:09 +0100

    Add Codecast 7.6

---
+++ ext/codecast/7.6/codecast-loader.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+  if (window.taskData || window.taskSettings) {
+    if (window.taskData && window.taskData.waitInit) {
+      window.taskData.waitInit();
+    }
+
+    var taskInstructionsHtml = $('#taskIntro').html();
+    var taskSolution = $('#solution').html();
+
+    var hints = $('#taskHints > div').toArray().map(elm => {
+      return {
+        content: elm.innerHTML.trim(),
+        minScore: elm.getAttribute('data-min-score') ? Number(elm.getAttribute('data-min-score')) : undefined,
+        id: elm.getAttribute('data-id') ? elm.getAttribute('data-id') : undefined,
+        question: elm.hasAttribute('data-question'),
+        previousHintId: elm.getAttribute('data-previous-hint-id') ? elm.getAttribute('data-previous-hint-id') : undefined,
+        nextHintId: elm.getAttribute('data-next-hint-id') ? elm.getAttribute('data-next-hint-id') : undefined,
+        yesHintId: elm.getAttribute('data-yes-hint-id') ? elm.getAttribute('data-yes-hint-id') : undefined,
+        noHintId: elm.getAttribute('data-no-hint-id') ? elm.getAttribute('data-no-hint-id') : undefined,
+        disablePrevious: elm.hasAttribute('data-disable-previous'),
+        disableNext: elm.hasAttribute('data-disable-next'),
+        immediate: elm.hasAttribute('data-immediate'),
+        levels: elm.hasAttribute('data-levels') ? elm.getAttribute('data-levels').split() : undefined,
+      };
+    });
+
+    var additionalOptions = window.taskData && window.taskData.codecastParameters ? window.taskData.codecastParameters : {};
+    if (window.codecastPreload) {
+      additionalOptions.preload = true;
+    }
+    if (!additionalOptions.language && window.stringsLanguage) {
+      additionalOptions.language = window.stringsLanguage;
+    }
+
+    var codecastParameters = $.extend(true, {
+      start: 'task',
+      showStepper: true,
+      showStack: true,
+      showViews: true,
+      showIO: true,
+      showDocumentation: true,
+      showFullScreen: true,
+      showMenu: true,
+      canRecord: false,
+      platform: 'python',
+      canChangePlatform: true,
+      canChangeLanguage: true,
+      canDownload: true,
+      controls: {},
+      audioWorkerUrl: window.modulesPath + "ext/codecast/7.6/index.worker.worker.js",
+      baseUrl: "https://codecast.france-ioi.org/v7",
+      authProviders: ["algorea", "guest"],
+      task: window.taskData,
+      taskInstructions: taskInstructionsHtml,
+      taskHints: hints,
+      taskSolution: taskSolution,
+      taskSuccessMessage: $('#taskSuccessMessage').length ? $('#taskSuccessMessage').html() : null,
+    }, additionalOptions, window.codecastPreload ? window.codecastPreload : {});
+
+    Codecast.start(codecastParameters);
+  }
+});
